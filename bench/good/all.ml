61305a13687be2d11275fecad2beee8ff647cb2b
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2002-03-05 12:42:43 +0000

    bench (all.ml) + Makefile
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@58 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/good/all.ml
@@ -0,0 +1,31 @@
+
+(* Types syntax *)
+
+external v : foo
+
+(* values *)
+external v1 : bool ref
+external v2 : int
+external v3 : (int)
+external v4 : (int) ref
+external v5 : foo
+external v6 : array 10 of int
+external v7 : array 10 of (int)
+external v8 : array 3+4 of (int -> int)
+
+(* functions *)
+external f1 : int -> bool -> int
+
+external f2 : int -> int ref -> bool
+
+external f3 : x:int ref -> y:int ref -> 
+             { p(x,y) } returns r:int reads x,y writes y { q(r,x,y,z) }
+
+external f4 : unit -> {} unit {}
+
+external f5 : foo -> foo
+external f6 : x:foo -> foo
+xexternal f7 : x:foo -> {} foo {}
+
+external f8 : t:array 10 of int -> {} unit { access(t,1) = 2 }
+
