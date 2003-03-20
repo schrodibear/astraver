8599fcdffdb78916abb24a7559a01012fd7d308a
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2003-03-20 16:57:02 +0000

    encore plus horrible
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@479 ecafba89-7d34-45a1-8a0b-92829e960b72

--- bench/good/set_why.v
+++ bench/good/set_why.v
@@ -9,7 +9,7 @@ Require Why.
   (sig_2 Z bool [x0: Z][result: bool](`x0 = v` /\
    ((if result then `x0 = 0` else `x0 <> 0`)))).
 
-(* Why obligation from file "good/set.mlw", characters 208-209 *)
+(* Why obligation from file "set.mlw", characters 208-209 *)
 Lemma p_po_1 : 
   (x0: Z)
   (Test1: `x0 = 0` /\ `x0 <> 0`)
@@ -23,7 +23,7 @@ Save.
   (sig_2 Z bool [x0: Z][result: bool](`x0 = v` /\
    ((if result then `x0 <> 0` else `x0 = 0`)))).
 
-(* Why obligation from file "good/set.mlw", characters 372-465 *)
+(* Why obligation from file "set.mlw", characters 372-465 *)
 Lemma p2_po_1 : 
   (y: Z)
   (Pre6: `y >= 0`)
@@ -41,7 +41,7 @@ Proof.
 Unfold Zwf; Intuition.
 Save.
 
-(* Why obligation from file "good/set.mlw", characters 358-478 *)
+(* Why obligation from file "set.mlw", characters 358-478 *)
 Lemma p2_po_2 : 
   (y: Z)
   (Pre6: `y >= 0`)
@@ -57,7 +57,7 @@ Proof.
 Intuition.
 Save.
 
-(* Why obligation from file "good/set.mlw", characters 559-560 *)
+(* Why obligation from file "set.mlw", characters 559-560 *)
 Lemma p3_po_1 : 
   (y: Z)
   (Pre4: `y >= 0`)
@@ -73,5 +73,24 @@ Lemma p3_po_1 :
    else `y0 = 0`).
 Proof.
 Unfold Zwf; Destruct b; Intuition.
+Save.
+
+(* Why obligation from file "set.mlw", characters 683-729 *)
+Lemma p4_po_1 : 
+  (y: Z)
+  (Pre4: `y >= 1`)
+  (Variant1: Z)
+  (y0: Z)
+  (Pre3: Variant1 = y0)
+  (Pre2: `y0 >= 1`)
+  (Test2: true = true)
+  (y1: Z)
+  (Post1: y1 = `y0 - 1`)
+  (x1: Z)
+  (result1: bool)
+  (Post10: `x1 = y1` /\ ((if result1 then `x1 <> 0` else `x1 = 0`)))
+  (if result1 then `y1 >= 1` /\ (Zwf `0` y1 y0) else `y1 = 0`).
+Proof.
+Unfold Zwf; Destruct result1; Intuition.
 Save.
 
