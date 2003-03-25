442d8f46e700fb66d7cfa35fb4a245e2df5f5196
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2003-03-25 16:43:35 +0000

    algos string matching
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@486 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ examples/string-matching/brute_force.c
@@ -0,0 +1,24 @@
+
+/* Brute force string matching */
+
+/*W logic match : int array, int, int array, int, int -> prop */
+
+void OUTPUT(char x[], char y[], int j) 
+     /*@ pre match(x,0,y,j,array_length(x)) reads x,y */;
+
+void BF(char x[], int m, char y[], int n) 
+/*@ array_length(x) = m and array_length(y) = n */
+{
+  int i = 0, j = 0;
+
+  for (j = 0; j <= n - m; ++j) 
+  /*@ invariant 0 <= j <= n - m + 1  variant n - m + 1 - j */
+  {
+    for (i = 0; i < m && x[i] == y[i + j]; ++i) 
+    /*@ invariant 0 <= i <= m and match(x,0,y,j,i)  variant m - i */;
+    if (i >= m)
+      OUTPUT(x,y,j);
+  }
+}
+   
+
