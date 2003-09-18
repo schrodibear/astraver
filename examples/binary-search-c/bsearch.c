bc23ace9ecd2d0a7e509ca9b9a03b756d5b4a953
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2003-09-18 16:06:50 +0000

    binary search en C
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@554 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ examples/binary-search-c/bsearch.c
@@ -0,0 +1,31 @@
+
+/*W logic In : int array,int,int -> prop */
+
+int binary_search(int t[], int n, int v) 
+/*@ array_length(t) = n and n >= 0 and sorted_array(t,0,n-1) */
+{
+  int l,u,p,m;
+  l = 1; 
+  u = n-1; 
+  p = -1;
+  while (l <= u) 
+    /*@ invariant 
+	  0 <= l and u <= array_length(t)-1 and -1 <= p <= array_length(t)-1
+          and (p = -1 -> In(t,0,array_length(t)-1) -> In(t,l,u))
+          and (p >= 0 -> t[p]=v)
+        variant 2+u-l */ {
+    m = (l + u) / 2;
+    /*@ assert l <= m and m <= u */;
+    if (t[m] < v)
+      l = m + 1;
+    else if (t[m] > v)
+      u = m - 1;
+    else
+      return m;
+  }
+  return -1;
+}
+/*@ (0 <= result <= array_length(t)-1 and t[result]=v) or 
+    (result = -1 and not In(t,1,array_length(t)-1)) */
+
+
