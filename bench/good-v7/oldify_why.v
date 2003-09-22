3e1d07dd6b020eb0f0d789f32bac37f6b0140098
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2003-09-22 21:25:55 +0000

    sauvegarde preuves en V7
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@567 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/good-v7/oldify_why.v
@@ -0,0 +1,16 @@
+
+Require Why.
+
+Parameter q : (array Z) -> (array Z) -> Z -> Prop.
+
+Parameter q1 : Z -> Z -> Z -> Prop.
+
+(*Why*) Parameter f1 :
+  (y: Z)(r: Z)(sig_2 Z unit [r0: Z][result: unit]((q1 r0 r y))).
+
+
+(*Why*) Parameter f :
+  (x: Z)(t: (array Z))
+  (sig_2 (array Z) unit [t0: (array Z)][result: unit]((q t0 t x))).
+
+
