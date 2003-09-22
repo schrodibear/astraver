cefabb7a4ca10dd6a9189538caf729b12cbb2a5f
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2003-09-22 21:26:32 +0000

    passage V8 dans good/: pour l'instant good/all
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@568 ecafba89-7d34-45a1-8a0b-92829e960b72

--- bench/good/all_why.v
+++ bench/good/all_why.v
@@ -1,6 +1,6 @@
 
-Require Why.
-Require Omega.
+Require Import Why.
+Require Import Omega.
 
 Parameter foo : Set.
 
@@ -10,127 +10,123 @@ Parameter foo : Set.
 
 (*Why*) Parameter v5 : foo.
 
-(*Why*) Parameter f1 : (_: Z)(_: bool)Z.
+(*Why*) Parameter f1 : forall (_: Z), forall (_: bool), Z.
 
-(*Why*) Parameter f2 : (_: Z)bool.
+(*Why*) Parameter f2 : forall (_: Z), bool.
 
 (*Why*) Parameter f3 :
-  (x: Z)(y: Z)(_: `x >= 0`)
-  (sig_2 Z Z [y0: Z][result: Z](`y0 = y + x + result`)).
+  forall (x: Z), forall (y: Z), forall (_: x >= 0),
+  (sig_2 Z Z (fun (y0: Z) (result: Z)  => (y0 = (y + x + result)))).
 
-(*Why*) Parameter f4 : (_: unit)unit.
+(*Why*) Parameter f4 : forall (_: unit), unit.
 
-(*Why*) Parameter f5 : (_: foo)foo.
+(*Why*) Parameter f5 : forall (_: foo), foo.
 
-(*Why*) Parameter f6 : (x: foo)foo.
+(*Why*) Parameter f6 : forall (x: foo), foo.
 
-(*Why*) Parameter f7 : (x: foo)foo.
+(*Why*) Parameter f7 : forall (x: foo), foo.
 
 (*Why*) Parameter f8 :
-  (t: (array Z))(sig_1 unit [result: unit](`(access t 1) = 2`)).
+  forall (t: (array Z)),
+  (sig_1 unit (fun (result: unit)  => ((access t 1) = 2))).
 
-
-
-
-
-(* Why obligation from file "good/all.mlw", characters 709-727 *)
+(* Why obligation from file "all.mlw", characters 709-727 *)
 Lemma p2_po_1 : 
   ~False.
 Proof.
-Tauto.
-Save.
+tauto.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 737-759 *)
+(* Why obligation from file "all.mlw", characters 737-759 *)
 Lemma p3_po_1 : 
   True /\ True.
 Proof.
-Tauto.
-Save.
+tauto.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 769-791 *)
+(* Why obligation from file "all.mlw", characters 769-791 *)
 Lemma p4_po_1 : 
   True \/ False.
 Proof.
-Tauto.
-Save.
+tauto.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 801-828 *)
+(* Why obligation from file "all.mlw", characters 801-828 *)
 Lemma p5_po_1 : 
   False \/ ~False.
-Proof. 
-Auto.
-Save.
+auto.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 838-864 *)
+(* Why obligation from file "all.mlw", characters 838-864 *)
 Lemma p6_po_1 : 
   (True -> ~False).
 Proof.
-Auto.
-Save.
+auto.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 874-900 *)
+(* Why obligation from file "all.mlw", characters 874-900 *)
 Lemma p7_po_1 : 
-  ((x:Z) `x = x`).
+  (forall (x:Z), x = x).
 Proof.
-Auto.
-Save.
+auto.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 910-945 *)
+(* Why obligation from file "all.mlw", characters 910-945 *)
 Lemma p8_po_1 : 
-  True /\ ((x:Z) `x = x`).
+  True /\ (forall (x:Z), x = x).
 Proof.
-Auto.
-Save.
+auto.
+Qed.
 
 
-(* Why obligation from file "good/all.mlw", characters 955-1002 *)
+(* Why obligation from file "all.mlw", characters 955-1002 *)
 Lemma p9_po_1 : 
-  ((x:Z) ((y:Z) (`x = y` -> `x = y`))).
+  (forall (x:Z), (forall (y:Z), (x = y -> x = y))).
 Proof.
-Trivial.
-Save.
+trivial.
+Qed.
 
-(* Why obligation from file "good/all.mlw", characters 1198-1201 *)
+(* Why obligation from file "all.mlw", characters 1198-1201 *)
 Lemma ar6_po_1 : 
-  ~(`1` = `0`).
+  ~(1 = 0).
 Proof.
-Omega.
-Save.
+omega.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 1212-1215 *)
+(* Why obligation from file "all.mlw", characters 1212-1215 *)
 Lemma ar7_po_1 : 
-  ~(`1` = `0`).
+  ~(1 = 0).
 Proof.
-Omega.
-Save.
+omega.
+Qed.
 
 
 
@@ -244,121 +240,112 @@ Save.
 
 
 
-(* Why obligation from file "good/all.mlw", characters 2156-2190 *)
+(* Why obligation from file "all.mlw", characters 2156-2190 *)
 Lemma arr1_po_1 : 
-  (v6: (array Z))
-  (Pre2: `(array_length v6) >= 1`)
-  `0 <= 0` /\ `0 < (array_length v6)`.
-Proof. (* arr1_po_1 *)
-Intros; Omega.
-Save.
+  forall (v6: (array Z)),
+  forall (Pre2: (array_length v6) >= 1),
+  0 <= 0 /\ 0 < (array_length v6).
+intros; omega.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 2203-2239 *)
+(* Why obligation from file "all.mlw", characters 2203-2239 *)
 Lemma arr2_po_1 : 
-  (v6: (array Z))
-  (Pre2: `(array_length v6) >= 4`)
-  `0 <= 1 + 2` /\ `1 + 2 < (array_length v6)`.
-Proof. (* arr2_po_1 *)
-Intros; Omega.
-Save.
+  forall (v6: (array Z)),
+  forall (Pre2: (array_length v6) >= 4),
+  0 <= (1 + 2) /\ (1 + 2) < (array_length v6).
+intros; omega.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 2251-2298 *)
+(* Why obligation from file "all.mlw", characters 2251-2298 *)
 Lemma arr3_po_1 : 
-  (v4: Z)
-  (v6: (array Z))
-  (Pre2: `(array_length v6) >= 1` /\ `v4 = 0`)
-  `0 <= v4` /\ `v4 < (array_length v6)`.
-Proof. (* arr3_po_1 *)
-Intros; Omega.
-Save.
+  forall (v4: Z),
+  forall (v6: (array Z)),
+  forall (Pre2: (array_length v6) >= 1 /\ v4 = 0),
+  0 <= v4 /\ v4 < (array_length v6).
+intros; omega.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 2354-2359 *)
+(* Why obligation from file "all.mlw", characters 2354-2359 *)
 Lemma arr4_po_1 : 
-  (v6: (array Z))
-  (Pre3: `(array_length v6) >= 10` /\ `(access v6 0) = 9`)
-  `0 <= 0` /\ `0 < (array_length v6)`.
-Proof. (* arr4_po_1 *)
-Intros; Omega.
-Save.
+  forall (v6: (array Z)),
+  forall (Pre3: (array_length v6) >= 10 /\ (access v6 0) = 9),
+  0 <= 0 /\ 0 < (array_length v6).
+intros; omega.
+Qed.
 
-(* Why obligation from file "good/all.mlw", characters 2310-2363 *)
+(* Why obligation from file "all.mlw", characters 2310-2363 *)
 Lemma arr4_po_2 : 
-  (v6: (array Z))
-  (Pre3: `(array_length v6) >= 10` /\ `(access v6 0) = 9`)
-  (Pre2: `0 <= 0` /\ `0 < (array_length v6)`)
-  `0 <= (access v6 0)` /\ `(access v6 0) < (array_length v6)`.
-Proof. (* arr4_po_2 *)
-Intros; Omega.
-Save.
+  forall (v6: (array Z)),
+  forall (Pre3: (array_length v6) >= 10 /\ (access v6 0) = 9),
+  forall (Pre2: 0 <= 0 /\ 0 < (array_length v6)),
+  0 <= (access v6 0) /\ (access v6 0) < (array_length v6).
+intros; omega.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 2376-2415 *)
+(* Why obligation from file "all.mlw", characters 2376-2415 *)
 Lemma arr5_po_1 : 
-  (v6: (array Z))
-  (Pre2: `(array_length v6) >= 1`)
-  `0 <= 0` /\ `0 < (array_length v6)`.
-Proof. (* arr5_po_1 *)
-Intros; Simpl; Omega.
-Save.
+  forall (v6: (array Z)),
+  forall (Pre2: (array_length v6) >= 1),
+  0 <= 0 /\ 0 < (array_length v6).
+intros; simpl; omega.
+Qed.
 
 
-(* Why obligation from file "good/all.mlw", characters 2427-2470 *)
+(* Why obligation from file "all.mlw", characters 2427-2470 *)
 Lemma arr6_po_1 : 
-  (v6: (array Z))
-  (Pre2: `(array_length v6) >= 4`)
-  `0 <= 1 + 2` /\ `1 + 2 < (array_length v6)`.
-Proof. (* arr6_po_1 *)
-Intros; Simpl; Omega.
-Save.
+  forall (v6: (array Z)),
+  forall (Pre2: (array_length v6) >= 4),
+  0 <= (1 + 2) /\ (1 + 2) < (array_length v6).
+intros; simpl; omega.
+Qed.
 
 
 
-(* Why obligation from file "good/all.mlw", characters 2482-2540 *)
+(* Why obligation from file "all.mlw", characters 2482-2540 *)
 Lemma arr7_po_1 : 
-  (v6: (array Z))
-  (Pre3: `(array_length v6) >= 10` /\ `(access v6 0) = 9`)
-  `0 <= (access v6 0)` /\ `(access v6 0) < (array_length v6)`.
-Proof. (* arr7_po_1 *)
-Intros; Omega.
-Save.
+  forall (v6: (array Z)),
+  forall (Pre3: (array_length v6) >= 10 /\ (access v6 0) = 9),
+  0 <= (access v6 0) /\ (access v6 0) < (array_length v6).
+intros; omega.
+Qed.
 
-(* Why obligation from file "good/all.mlw", characters 2526-2531 *)
+(* Why obligation from file "all.mlw", characters 2526-2531 *)
 Lemma arr7_po_2 : 
-  (v6: (array Z))
-  (Pre3: `(array_length v6) >= 10` /\ `(access v6 0) = 9`)
-  (Pre2: `0 <= (access v6 0)` /\ `(access v6 0) < (array_length v6)`)
-  `0 <= 0` /\ `0 < (array_length v6)`.
-Proof. (* arr7_po_2 *)
-Intros; Simpl; Omega.
-Save.
+  forall (v6: (array Z)),
+  forall (Pre3: (array_length v6) >= 10 /\ (access v6 0) = 9),
+  forall (Pre2: 0 <= (access v6 0) /\ (access v6 0) < (array_length v6)),
+  0 <= 0 /\ 0 < (array_length v6).
+intros; simpl; omega.
+Qed.
 
 
 
 
 
-(* Why obligation from file "good/all.mlw", characters 2645-2653 *)
+(* Why obligation from file "all.mlw", characters 2645-2653 *)
 Lemma fc3_po_1 : 
-  (a: Z)
-  (Post2: a = `0`)
-  (b: Z)
-  (Post1: b = `0`)
-  `a >= 0`.
-Proof. Intros; Omega. Save.
+  forall (a: Z),
+  forall (Post2: a = 0),
+  forall (b: Z),
+  forall (Post1: b = 0),
+  a >= 0.
+ intros; omega. Qed.
 
 
 
@@ -368,60 +355,59 @@ Proof. Intros; Omega. Save.
 
 
 
-(* Why obligation from file "good/all.mlw", characters 2796-2844 *)
+(* Why obligation from file "all.mlw", characters 2796-2844 *)
 Lemma an2_po_1 : 
-  (v4: Z)
-  (Pre1: `v4 >= 0`)
-  (v4_0: Z)
-  (Post1: v4_0 = `v4 + 1`)
-  `v4_0 > v4`.
+  forall (v4: Z),
+  forall (Pre1: v4 >= 0),
+  forall (v4_0: Z),
+  forall (Post1: v4_0 = (v4 + 1)),
+  v4_0 > v4.
 Proof.
-Intros; Omega.
-Save.
+intros; omega.
+Qed.
 
 
-(*Why*) Inductive ET_E1 [T:Set] : Set :=
-  | Val_E1 : T -> (ET_E1 T)
-  | Exn_E1 : (ET_E1 T).
+(*Why*) Inductive ET_E1 (T:Set) : Set :=
+          | Val_E1 : T -> ET_E1 T
+          | Exn_E1 : ET_E1 T.
 
-(*Why*) Definition post_E1 :=
-  [T:Set][P:Prop][Q:T->Prop][x:(ET_E1 T)]
-  Cases x of 
-  | (Val_E1 v) => (Q v)
-  | Exn_E1 => P
-  end.
+(*Why*) Definition post_E1 (T:Set) (P:Prop) (Q:T -> Prop)
+          (x:ET_E1 T) :=
+          match x with
+          | Val_E1 v => Q v
+          | Exn_E1 => P
+          end.
 
-(*Why*) Implicits post_E1.
+(*Why*) Implicit Arguments post_E1.
 
-(*Why*) Inductive ET_E2 [T:Set] : Set :=
-  | Val_E2 : T -> (ET_E2 T)
-  | Exn_E2 : Z -> (ET_E2 T).
+(*Why*) Inductive ET_E2 (T:Set) : Set :=
+          | Val_E2 : T -> ET_E2 T
+          | Exn_E2 : Z -> ET_E2 T.
 
-(*Why*) Definition post_E2 :=
-  [T:Set][P:Z -> Prop][Q:T->Prop][x:(ET_E2 T)]
-  Cases x of 
-  | (Val_E2 v) => (Q v)
-  | (Exn_E2 v) => (P v)
-  end.
+(*Why*) Definition post_E2 (T:Set) (P:Z -> Prop) (Q:T -> Prop)
+          (x:ET_E2 T) :=
+          match x with
+          | Val_E2 v => Q v
+          | Exn_E2 v => P v
+          end.
 
-(*Why*) Implicits post_E2.
+(*Why*) Implicit Arguments post_E2.
 
-(*Why*) Inductive ET_E3 [T:Set] : Set :=
-  | Val_E3 : T -> (ET_E3 T)
-  | Exn_E3 : foo -> (ET_E3 T).
+(*Why*) Inductive ET_E3 (T:Set) : Set :=
+          | Val_E3 : T -> ET_E3 T
+          | Exn_E3 : foo -> ET_E3 T.
 
-(*Why*) Definition post_E3 :=
-  [T:Set][P:foo -> Prop][Q:T->Prop][x:(ET_E3 T)]
-  Cases x of 
-  | (Val_E3 v) => (Q v)
-  | (Exn_E3 v) => (P v)
-  end.
+(*Why*) Definition post_E3 (T:Set) (P:foo -> Prop) (Q:T -> Prop)
+          (x:ET_E3 T) :=
+          match x with
+          | Val_E3 v => Q v
+          | Exn_E3 v => P v
+          end.
 
-(*Why*) Implicits post_E3.
+(*Why*) Implicit Arguments post_E3.
 
 
 
-(*Why*) Parameter f1_ex : (n: Z)(EM unit unit).
+(*Why*) Parameter f1_ex : forall (n: Z), (EM unit unit).
 
-(*Why*) Parameter f2_ex : (x: Z)(tuple_2 Z (EM unit (EM Z bool))).
-
+(*Why*) Parameter f2_ex : forall (x: Z), (tuple_2 Z (EM unit (EM Z bool))).
