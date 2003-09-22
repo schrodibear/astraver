176d131f88e01ab108ab3a59b9de0850c4ec2266
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2003-09-22 12:22:37 +0000

    Coq V7
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@562 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ lib/coq-v7/Why.v
@@ -0,0 +1,34 @@
+(*
+ * The Why certification tool
+ * Copyright (C) 2002 Jean-Christophe FILLIATRE
+ * 
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU General Public License version 2 for more details
+ * (enclosed in the file GPL).
+ *)
+
+(* $Id: Why.v,v 1.1 2003-09-22 12:22:36 filliatr Exp $ *)
+
+Require Export WhyCoqCompat.
+
+Require Export WhyTuples.
+Require Export WhyInt.
+Require Export WhyBool.
+Require Export WhyArrays.
+Require Export WhyPermut.
+Require Export WhySorted.
+Require Export WhyTactics.
+Require Export WhyExn.
+Require Export WhyLemmas.
+
+Require Export WhyCM.
+
+Implicits well_founded [1].
+Hints Unfold Zwf.
