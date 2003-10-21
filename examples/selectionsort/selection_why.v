305fc5762a313b9c57ad1b9d702a838f0d8e7346
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2003-10-21 15:26:30 +0000

    quantif prenexe pour Simplify
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@608 ecafba89-7d34-45a1-8a0b-92829e960b72

--- examples/selectionsort/selection_why.v
+++ examples/selectionsort/selection_why.v
@@ -18,8 +18,8 @@ Lemma selection_po_1 :
   forall (Pre17: (0 <= i1 /\ i1 <= ((array_length t0) - 1)) /\
                  (sorted_array t0 0 (i1 - 1)) /\ (permut t0 t) /\
                  (forall (k:Z),
-                  (0 <= k /\ k < i1 ->
-                   (forall (l:Z),
+                  (forall (l:Z),
+                   (0 <= k /\ k < i1 ->
                     (i1 <= l /\ l < (array_length t0) -> (access t0 k) <=
                      (access t0 l)))))),
   forall (Test6: i1 < ((array_length t0) - 1)),
@@ -54,8 +54,8 @@ Lemma selection_po_2 :
   forall (Pre17: (0 <= i1 /\ i1 <= ((array_length t0) - 1)) /\
                  (sorted_array t0 0 (i1 - 1)) /\ (permut t0 t) /\
                  (forall (k:Z),
-                  (0 <= k /\ k < i1 ->
-                   (forall (l:Z),
+                  (forall (l:Z),
+                   (0 <= k /\ k < i1 ->
                     (i1 <= l /\ l < (array_length t0) -> (access t0 k) <=
                      (access t0 l)))))),
   forall (Test6: i1 < ((array_length t0) - 1)),
@@ -91,8 +91,8 @@ Lemma selection_po_3 :
   forall (Pre17: (0 <= i1 /\ i1 <= ((array_length t0) - 1)) /\
                  (sorted_array t0 0 (i1 - 1)) /\ (permut t0 t) /\
                  (forall (k:Z),
-                  (0 <= k /\ k < i1 ->
-                   (forall (l:Z),
+                  (forall (l:Z),
+                   (0 <= k /\ k < i1 ->
                     (i1 <= l /\ l < (array_length t0) -> (access t0 k) <=
                      (access t0 l)))))),
   forall (Test6: i1 < ((array_length t0) - 1)),
@@ -143,8 +143,8 @@ Lemma selection_po_4 :
   forall (Pre17: (0 <= i1 /\ i1 <= ((array_length t0) - 1)) /\
                  (sorted_array t0 0 (i1 - 1)) /\ (permut t0 t) /\
                  (forall (k:Z),
-                  (0 <= k /\ k < i1 ->
-                   (forall (l:Z),
+                  (forall (l:Z),
+                   (0 <= k /\ k < i1 ->
                     (i1 <= l /\ l < (array_length t0) -> (access t0 k) <=
                      (access t0 l)))))),
   forall (Test6: i1 < ((array_length t0) - 1)),
@@ -190,8 +190,8 @@ Lemma selection_po_5 :
   forall (Pre17: (0 <= i1 /\ i1 <= ((array_length t0) - 1)) /\
                  (sorted_array t0 0 (i1 - 1)) /\ (permut t0 t) /\
                  (forall (k:Z),
-                  (0 <= k /\ k < i1 ->
-                   (forall (l:Z),
+                  (forall (l:Z),
+                   (0 <= k /\ k < i1 ->
                     (i1 <= l /\ l < (array_length t0) -> (access t0 k) <=
                      (access t0 l)))))),
   forall (Test6: i1 < ((array_length t0) - 1)),
@@ -222,8 +222,8 @@ Lemma selection_po_6 :
   forall (Pre17: (0 <= i1 /\ i1 <= ((array_length t0) - 1)) /\
                  (sorted_array t0 0 (i1 - 1)) /\ (permut t0 t) /\
                  (forall (k:Z),
-                  (0 <= k /\ k < i1 ->
-                   (forall (l:Z),
+                  (forall (l:Z),
+                   (0 <= k /\ k < i1 ->
                     (i1 <= l /\ l < (array_length t0) -> (access t0 k) <=
                      (access t0 l)))))),
   forall (Test6: i1 < ((array_length t0) - 1)),
@@ -256,8 +256,8 @@ Lemma selection_po_7 :
   forall (Pre17: (0 <= i1 /\ i1 <= ((array_length t0) - 1)) /\
                  (sorted_array t0 0 (i1 - 1)) /\ (permut t0 t) /\
                  (forall (k:Z),
-                  (0 <= k /\ k < i1 ->
-                   (forall (l:Z),
+                  (forall (l:Z),
+                   (0 <= k /\ k < i1 ->
                     (i1 <= l /\ l < (array_length t0) -> (access t0 k) <=
                      (access t0 l)))))),
   forall (Test6: i1 < ((array_length t0) - 1)),
@@ -294,8 +294,8 @@ Lemma selection_po_8 :
   forall (Pre17: (0 <= i1 /\ i1 <= ((array_length t0) - 1)) /\
                  (sorted_array t0 0 (i1 - 1)) /\ (permut t0 t) /\
                  (forall (k:Z),
-                  (0 <= k /\ k < i1 ->
-                   (forall (l:Z),
+                  (forall (l:Z),
+                   (0 <= k /\ k < i1 ->
                     (i1 <= l /\ l < (array_length t0) -> (access t0 k) <=
                      (access t0 l)))))),
   forall (Test6: i1 < ((array_length t0) - 1)),
@@ -336,8 +336,8 @@ Lemma selection_po_9 :
   forall (Pre17: (0 <= i1 /\ i1 <= ((array_length t0) - 1)) /\
                  (sorted_array t0 0 (i1 - 1)) /\ (permut t0 t) /\
                  (forall (k:Z),
-                  (0 <= k /\ k < i1 ->
-                   (forall (l:Z),
+                  (forall (l:Z),
+                   (0 <= k /\ k < i1 ->
                     (i1 <= l /\ l < (array_length t0) -> (access t0 k) <=
                      (access t0 l)))))),
   forall (Test6: i1 < ((array_length t0) - 1)),
@@ -366,8 +366,8 @@ Lemma selection_po_9 :
    (i = (i1 + 1) -> ((0 <= i /\ i <= ((array_length t2) - 1)) /\
     (sorted_array t2 0 (i - 1)) /\ (permut t2 t) /\
     (forall (k:Z),
-     (0 <= k /\ k < i ->
-      (forall (l:Z),
+     (forall (l:Z),
+      (0 <= k /\ k < i ->
        (i <= l /\ l < (array_length t2) -> (access t2 k) <= (access t2 l)))))) /\
     (Zwf 0 ((array_length t2) - i) ((array_length t0) - i1)))).
 Proof.
@@ -439,8 +439,8 @@ Lemma selection_po_10 :
   (0 <= i /\ i <= ((array_length t) - 1)) /\ (sorted_array t 0 (i - 1)) /\
   (permut t t) /\
   (forall (k:Z),
-   (0 <= k /\ k < i ->
-    (forall (l:Z),
+   (forall (l:Z),
+    (0 <= k /\ k < i ->
      (i <= l /\ l < (array_length t) -> (access t k) <= (access t l))))).
 Proof.
 intuition.
@@ -458,8 +458,8 @@ Lemma selection_po_11 :
   forall (Post10: ((0 <= i1 /\ i1 <= ((array_length t0) - 1)) /\
                   (sorted_array t0 0 (i1 - 1)) /\ (permut t0 t) /\
                   (forall (k:Z),
-                   (0 <= k /\ k < i1 ->
-                    (forall (l:Z),
+                   (forall (l:Z),
+                    (0 <= k /\ k < i1 ->
                      (i1 <= l /\ l < (array_length t0) -> (access t0 k) <=
                       (access t0 l)))))) /\
                   i1 >= ((array_length t0) - 1)),
