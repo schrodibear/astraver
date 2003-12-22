8b2d43978b5be1ed77b1fd639710e6674e2ff828
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2003-12-22 15:50:32 +0000

    fichiers pour les types
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@655 ecafba89-7d34-45a1-8a0b-92829e960b72

--- bench/c/good/syntax/types.c
+++ bench/c/good/syntax/types.c
@@ -1,1 +1,54 @@
 
+/* basic types */
+
+char a;
+short c;
+int b;
+long d;
+long long e;
+float f;
+double g;
+long double h;
+
+void i;
+
+/* arrays */
+
+char j[5];
+long l[5][6];
+
+/* pointers */
+
+int * m;
+short ** n;
+
+/* type specifiers */
+
+extern int o;
+const char p;
+signed char q;
+unsigned int r;
+signed s; /* defaults to signed int */
+
+/* structures */
+
+struct T; /* forward declaration */
+struct S { int fa; }; 
+struct T { char fb; float fc; };
+struct { int fd :3; } sa; /* bit field */
+
+/* unions */
+
+/* enums */
+
+/* nightmare examples from K&R, section 5.12 */
+
+int (*kra)[13];
+int *krb[13];
+int *krc();
+int (*krd)();
+char (*(*kre())[])();
+char (*(*krf[3])())[5];
+
+/* typedefs */
+
