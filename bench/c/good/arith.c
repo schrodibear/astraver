a671bc3ca8b89db06b44e1328858c41cd7730429
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2004-02-03 08:24:43 +0000

    add info for variables, more interp
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@697 ecafba89-7d34-45a1-8a0b-92829e960b72

--- bench/c/good/arith.c
+++ bench/c/good/arith.c
@@ -12,12 +12,13 @@ void test(int k)
 /*@ true */
 { 
   int l = 1;
+  int m = 12;
   i = j + k;
-  l = l * j;
-  j = j + l + 10 * k +i;
+  l = l * j ;
+  j = j + l + 10 * k + i + m;
 }
 /*@ true */
 
-/* i == j@ + k and j == 3 * j@ + 11 * k *) */
+/* i == j@ + k and j == 3 * j@ + 11 * k + 12 */
 
 
