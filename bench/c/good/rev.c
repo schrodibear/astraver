3464a65e6d3031c07cfb5471500342a884ab5678
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2004-03-03 07:56:02 +0000

    in-place list reversal
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@745 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/c/good/rev.c
@@ -0,0 +1,68 @@
+
+/* in-place list reversal */
+
+typedef struct struct_list {
+  int hd;
+  struct struct_list * tl;
+} *list;
+
+#define nil ((void*)0)
+
+/*@ predicate is_list(list l) reads l->tl (*???*) */
+/*@ predicate llist(list l, plist pl) */
+/*@ logic plist rev(plist pl) */
+/*@ logic plist app(plist l1, plist l2) */
+/*@ predicate eq_list(plist l1, plist l2) */
+/*@ predicate disjoint(plist l1, plist l2) */
+
+/*@ requires is_list(p)
+  @ (* assigns ??? *)
+  @ ensures \exists plist l0; llist(\result, l0) && llist(\old(p), rev(l0))
+  @*/
+list rev(list p) {
+  list r = p;
+  p = nil;
+  /*@ invariant 
+        \exists plist lp; \exists plist lr;
+          llist(p, lp) && llist(r, lr) && disjoint(lp, lr) &&
+          \forall plist l; 
+            llist(\old(p), l) => eq_list(app(rev(lr), lp), rev(l))
+    @ variant r for ll_order */
+  while (r != nil) {
+    list q = r;
+    r = r->tl;
+    q->tl = p;
+    p = q;
+  }
+  return p;
+}
+
+/* test */
+
+#if 1
+#include <stdio.h>
+
+void print(list l) {
+  if (l == nil) 
+    printf("nil\n");
+  else {
+    printf("%d :: ", l->hd);
+    print(l->tl);
+  }
+}
+
+int main() {
+  /* 1::2::3::nil */
+  struct struct_list l[3];
+  list r;
+  l[0].hd = 1;
+  l[0].tl = &l[1];
+  l[1].hd = 2;
+  l[1].tl = &l[2];
+  l[2].hd = 3;
+  l[2].tl = nil;
+  print(l);
+  r = rev(l);
+  print(r);
+}
+#endif
