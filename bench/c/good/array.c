0645ec884e6c8933b8fd21e03008e2cabcd12ac7
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2004-04-07 15:46:18 +0000

    version 0.52
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@891 ecafba89-7d34-45a1-8a0b-92829e960b72

--- bench/c/good/array.c
+++ bench/c/good/array.c
@@ -3,3 +3,7 @@ int t[3][3];
 
 //@ requires \valid_index(t,1) && \valid_index(t[1],2)
 int getcell() { return t[1][2];  }
+
+/*@ predicate valid_dim2(int** t, int i, int j, int k, int l)
+    { \valid_range(t,i,j) &&
+    \forall int n; i <= n <= j => \valid_range(t[n],k,l) } */
