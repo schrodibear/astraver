27fe136467a1a8a233ce81dbeb513e69ffe66a53
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2004-10-18 08:03:37 +0000

    *** empty log message ***
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@1023 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/c/good/sizeof.c
@@ -0,0 +1,10 @@
+
+/*@ ensures \result == 4 */
+int f1() { return sizeof(long int); }
+
+/*@ ensures \result == 34 */
+int f2() { return sizeof(short[17]); }
+
+typedef struct { int x :16; int y :16; } tau;
+/*@ ensures \result == 4 */
+int f3() { return sizeof(tau); }
