03d48f70ce2d6c07524c5ecd668d3e3d636cf736
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2004-11-23 16:17:32 +0000

    ifs
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@1080 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/c/good/ifs.c
@@ -0,0 +1,24 @@
+typedef struct {
+	int Pn;
+	int CPRE_Pn;
+	int Nivpn;
+	int CPRE_Nivpn;
+	int CPRE_VC[4];
+	int VC[4];
+	int * Cpt[4];
+	int Param;
+	int Param4_Pn;
+	int V;
+	} MaStruct;
+int Ch_Pn[4];
+int SPMEP[4];
+
+/*@ requires \valid(Parametre)
+  @*/
+void V4A (MaStruct *Parametre,int Val_Boitier [ 4 ],int Pn_Bac [ 4 ],MaStruct *Parametre_Ref)
+{
+  Parametre->VC[0] = ! ( Ch_Pn[0] || Pn_Bac[0] || SPMEP[0] );
+  Parametre->VC[1] = ! ( Ch_Pn[1] || Pn_Bac[1] || SPMEP[1] );
+  Parametre->VC[2] = ! ( Ch_Pn[2] || Pn_Bac[2] || SPMEP[2] );
+  Parametre->VC[3] = ! ( Parametre->Param4_Pn || Pn_Bac[3]);
+}
