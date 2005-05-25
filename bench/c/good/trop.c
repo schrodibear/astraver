031e3482b8ee71aca69be862cc69ca4b7e27ea70
hubert <hubert@ecafba89-7d34-45a1-8a0b-92829e960b72>
2005-05-25 12:23:44 +0000

    *** empty log message ***
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@1301 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/c/good/trop.c
@@ -0,0 +1,15 @@
+typedef struct las { int * p; int ta[1]; } las;
+
+las u,v;
+
+/*@
+requires \valid(u.p) && \base_addr(u.ta)!=\base_addr(u.p)
+assigns *u.p,u.ta[0]
+ensures u.ta[0]==0
+*/
+void f()
+{
+//	g();
+u.ta[0]=0;
+*u.p=5;
+}
