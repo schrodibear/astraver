20c1571ef554549a986b297fc2e13a10b7a86a4d
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2006-06-12 14:19:21 +0000

    pas d'encodage par defaut
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@1596 ecafba89-7d34-45a1-8a0b-92829e960b72

--- src/options.ml
+++ src/options.ml
@@ -14,7 +14,7 @@
  * (enclosed in the file GPL).
  *)
 
-(*i $Id: options.ml,v 1.62 2006-06-09 15:03:25 lescuyer Exp $ i*)
+(*i $Id: options.ml,v 1.63 2006-06-12 14:19:21 filliatr Exp $ i*)
 
 open Format
 
@@ -47,7 +47,7 @@ let prelude_ = ref true
 let gappa_rnd_ = ref "float < ieee_64, ne >"
 
 type encoding = NoEncoding | Predicates | Stratified | Recursive
-let types_encoding_ = ref Stratified
+let types_encoding_ = ref NoEncoding
 
 type termination = UseVariant | Partial | Total
 let termination_ = ref UseVariant
