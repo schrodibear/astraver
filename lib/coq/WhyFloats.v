9d545af06d913253382993f6de1b3d357e24eb23
sboldo <sboldo@ecafba89-7d34-45a1-8a0b-92829e960b72>
2006-06-20 15:19:25 +0000

    Single is nearly done.
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@1618 ecafba89-7d34-45a1-8a0b-92829e960b72

--- lib/coq/WhyFloats.v
+++ lib/coq/WhyFloats.v
@@ -3,72 +3,169 @@
    Implements the file lib/why/floats.why *)
 
 Require Export Reals.
+Add LoadPath "/users/demons/sboldo/Recherche/PFF/V8.0".
+Require Export AllFloat.
+Require Export "Others/RND".
 
-Definition mode: Set.
+Let radix := 2%Z.
+
+Section Utiles.
+
+Lemma radixGreaterOne: (1 < radix)%Z.
+auto with zarith.
+Qed.
+
+
+Definition nat_to_N (n:nat) := match n with 
+   |  0    => N0
+   | (S m) => (Npos (P_of_succ_nat m))
+   end.
+
+Lemma nat_to_N_correct: forall n:nat, Z_of_N (nat_to_N n)=n.
+intros.
+intros; induction n; simpl; auto.
+Qed.
+
+
+Definition make_bound (p E:nat) := Bound 
+      (P_of_succ_nat (pred (Zabs_nat (Zpower_nat radix p))))
+      (nat_to_N E).
+
+Lemma make_EGivesEmin: forall p E:nat, 
+        (Z_of_N (dExp (make_bound p E)))=E.
+intros; simpl; apply nat_to_N_correct.
+Qed.
+
+Lemma make_pGivesBound: forall p E:nat, 
+        Zpos (vNum (make_bound p E))=(Zpower_nat radix p).
+intros.
+unfold make_bound, vNum.
+apply
+ trans_eq
+  with
+    (Z_of_nat
+       (nat_of_P
+          (P_of_succ_nat
+             (pred (Zabs_nat (Zpower_nat radix p)))))).
+unfold Z_of_nat in |- *; rewrite nat_of_P_o_P_of_succ_nat_eq_succ;
+ auto with zarith.
+rewrite nat_of_P_o_P_of_succ_nat_eq_succ; auto with arith zarith.
+cut (Zabs (Zpower_nat radix p) = Zpower_nat radix p).
+intros H; pattern (Zpower_nat radix p) at 2 in |- *; rewrite <- H.
+rewrite Zabs_absolu.
+rewrite <- (S_pred (Zabs_nat (Zpower_nat radix p)) 0);
+ auto with arith zarith.
+apply lt_Zlt_inv; simpl in |- *; auto with zarith arith.
+rewrite <- Zabs_absolu; rewrite H; auto with arith zarith.
+apply Zabs_eq; auto with arith zarith.
+Qed.
+
+
+End Utiles.
+
+
+
+Inductive mode : Set := nearest_even | to_zero | up | down | nearest_away.
+
+Coercion Local FtoRradix := FtoR radix.
+
+(** Single precision *)
+
+Let bsingle := make_bound 24 149.
+
+Lemma psGreaterThanOne: 1 < 24.
+auto with arith.
+Qed.
+
+Lemma psGivesBound: Zpos (vNum bsingle) = Zpower_nat radix 24.
+unfold bsingle; apply make_pGivesBound.
+Qed.
+
+Record single : Set := mk_single {
+   s_float    : float;
+   s_canonic  : Fcanonic radix bsingle s_float;
+   s_to_exact : R;
+   s_to_model : R
+ }.
+
+
+Definition s_to_r (f:single) := FtoRradix (s_float f).
+
+Definition single_round_error (f:single):= 
+    (Rabs ((s_to_exact f) - (s_to_r f))).
+
+Definition single_total_error (f:single):= 
+    (Rabs ((s_to_model f) - (s_to_r f))).
+
+Definition single_set_model (f:single) (r:R) :=
+      mk_single (s_float f) (s_canonic f) (s_to_exact f) r.
+
+
+
+
+
+Definition r_to_s_aux (m:mode) (r r1 r2:R) := match m with
+  |  nearest_even => mk_single (RND_EvenClosest bsingle radix 24 r) 
+                     (RND_EvenClosest_canonic bsingle radix 24 
+                         radixGreaterOne psGreaterThanOne psGivesBound r)
+                     r1 r2
+  |  to_zero      => mk_single (RND_Zero bsingle radix 24 r) 
+                     (RND_Zero_canonic bsingle radix 24 
+                         radixGreaterOne psGreaterThanOne psGivesBound r)
+                     r1 r2
+  |  up           => mk_single (RND_Min bsingle radix 24 r) 
+                     (RND_Min_canonic bsingle radix 24 
+                         radixGreaterOne psGreaterThanOne psGivesBound r)
+                     r1 r2
+  |  down         => mk_single (RND_Max bsingle radix 24 r) 
+                     (RND_Max_canonic bsingle radix 24 
+                         radixGreaterOne psGreaterThanOne psGivesBound r)
+                     r1 r2
+  |  nearest_away => mk_single (RND_ClosestUp bsingle radix 24 r) 
+                     (RND_ClosestUp_canonic bsingle radix 24 
+                         radixGreaterOne psGreaterThanOne psGivesBound r)
+                     r1 r2
+  end.
+
+   
+Definition r_to_s (m:mode) (r:R) := r_to_s_aux m r r r.
+   
+
+Definition add_single (m:mode) (f1 f2:single) :=
+     r_to_s_aux m (s_float f1+s_float f2) 
+                  (s_to_exact f1+s_to_exact f2) (s_to_model f1+s_to_model f2).
+
+Definition sub_single (m:mode) (f1 f2:single) :=
+     r_to_s_aux m (s_float f1-s_float f2) 
+                  (s_to_exact f1-s_to_exact f2) (s_to_model f1-s_to_model f2).
+
+Definition mul_single (m:mode) (f1 f2:single) :=
+     r_to_s_aux m (s_float f1*s_float f2) 
+                  (s_to_exact f1*s_to_exact f2) (s_to_model f1*s_to_model f2).
+
+Definition div_single (m:mode) (f1 f2:single) :=
+     r_to_s_aux m (s_float f1/s_float f2) 
+                  (s_to_exact f1/s_to_exact f2) (s_to_model f1/s_to_model f2).
+
+Definition sqrt_single (m:mode) (f:single) :=
+     r_to_s_aux m (sqrt (s_float f))
+                  (sqrt(s_to_exact f)) (sqrt(s_to_model f)).
+
+Definition neg_single (m:mode) (f:single) :=
+   mk_single (Fopp (s_float f)) 
+         (FcanonicFopp radix bsingle (s_float f) (s_canonic f))
+         (-(s_to_exact f)) (-(s_to_model f)).
+
+Definition abs_single (m:mode) (f:single) :=
+   mk_single (Fabs (s_float f)) 
+         (FcanonicFabs radix radixGreaterOne bsingle (s_float f) (s_canonic f))
+         (Rabs (s_to_exact f)) (Rabs (s_to_model f)).
+
+Definition max_single : R.
 Admitted.
 
-Definition nearest_even : mode.
-Admitted.
+Let bdouble := make_bound 53 1074.
 
-Definition to_zero : mode.
-Admitted.
-
-Definition up : mode.
-Admitted.
-
-Definition down : mode.
-Admitted.
-
-Definition nearest_away : mode.
-Admitted.
-
-Definition single: Set.
-Admitted.
-
-Definition add_single : mode -> single -> single -> single.
-Admitted.
-
-Definition sub_single : mode -> single -> single -> single.
-Admitted.
-
-Definition mul_single : mode -> single -> single -> single.
-Admitted.
-
-Definition div_single : mode -> single -> single -> single.
-Admitted.
-
-Definition neg_single : mode -> single -> single.
-Admitted.
-
-Definition abs_single : mode -> single -> single.
-Admitted.
-
-Definition sqrt_single : mode -> single -> single.
-Admitted.
-
-Definition s_to_r : single -> R.
-Admitted.
-
-Definition s_to_exact : single -> R.
-Admitted.
-
-Definition s_to_model : single -> R.
-Admitted.
-
-Definition r_to_s : mode -> R -> single.
-Admitted.
-
-Definition single_round_error : single -> R.
-Admitted.
-
-Definition single_total_error : single -> R.
-Admitted.
-
-Definition single_set_model : single -> R -> single.
-Admitted.
-
-Definition max_single : mode -> R.
-Admitted.
 
 Definition double: Set.
 Admitted.
@@ -115,7 +212,7 @@ Admitted.
 Definition double_set_model : double -> R -> double.
 Admitted.
 
-Definition max_double : mode -> R.
+Definition max_double : R.
 Admitted.
 
 Definition quad: Set.
@@ -163,7 +260,7 @@ Admitted.
 Definition quad_set_model : quad -> R -> quad.
 Admitted.
 
-Definition max_quad : mode -> R.
+Definition max_quad : R.
 Admitted.
 
 Definition double_of_single : single -> double.
