b9bac32323be88641452dc37a48efab0a74430e6
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2006-07-20 11:27:11 +0000

    *** empty log message ***
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@1692 ecafba89-7d34-45a1-8a0b-92829e960b72

--- bench/c/good/overflow.c
+++ bench/c/good/overflow.c
@@ -1,29 +1,39 @@
+char c;
+signed char sc;
+char c1;
+short s;
 
 enum E1 { A1, B1 };
 enum E2 { A2, B2, C2 };
 
+enum E1 e1;
+enum E2 e2;
+
 /*@ requires e2 != C2 */
-void f(enum E1 e1, enum E2 e2) {
+void f() {
   e1 = e2;
 }
 
-/*@ requires s <= 255 */
-void g(unsigned short s) {
-  char c = s;
+/*@ requires 0 <= s <= 255 */
+void g() {
+  c = s;
 }
 
+int i;
+long int li;
+long long int lli;
+
 void h() {
-  char c = 10;
-  unsigned char uc = c;
-  char c1;
-  short s = c1;
-  int i = s;
-  long int li = i;
-  long long int lli = li;
+  c = 10;
+  sc = c;
+  s = c1;
+  i = s;
+  li = i;
+  lli = li;
 }
 
 void hh() {
-  int i = 7;
-  short s = i;
-  char c = s;
+  i = 7;
+  s = i;
+  c = s;
 }
