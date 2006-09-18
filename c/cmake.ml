1e2f7c5ea267b3cafb1f25c0c0312f38c4a59bca
couchot <couchot@ecafba89-7d34-45a1-8a0b-92829e960b72>
2006-09-18 13:34:00 +0000

    JFC: --encoding mono for the smtlib output
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@1704 ecafba89-7d34-45a1-8a0b-92829e960b72

--- c/cmake.ml
+++ c/cmake.ml
@@ -14,7 +14,7 @@
  * (enclosed in the file GPL).
  *)
 
-(*i $Id: cmake.ml,v 1.29 2006-09-18 12:22:56 couchot Exp $ i*)
+(*i $Id: cmake.ml,v 1.30 2006-09-18 13:34:00 couchot Exp $ i*)
 
 open Format
 open Pp
@@ -113,7 +113,7 @@ let generic f targets =
        fprintf fmt "smtlib: %a@\n" (print_files smtlib) targets;
        fprintf fmt "@\n";
        fprintf fmt "smtlib/%%_why.smt: why/%s_spec.why why/%%.why@\n" f;
-       fprintf fmt "\t@@echo 'why -smtlib [...] why/$*.why' && $(WHY) -smtlib -dir smtlib --encoding pred $(CADULIB)/why/$(CADULIBFILE) why/%s_spec.why why/$*.why@\n@\n" f;
+       fprintf fmt "\t@@echo 'why -smtlib [...] why/$*.why' && $(WHY) -smtlib -dir smtlib --encoding mono $(CADULIB)/why/$(CADULIBFILE) why/%s_spec.why why/$*.why@\n@\n" f;
        
        fprintf fmt "gui stat: %s@\n" 
 	 (match targets with f::_ -> f^".stat" | [] -> "");
