c023dfa3c6c40728566c8b2696cbffc1a4b5871a
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2006-10-30 15:39:33 +0000

    make
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@1785 ecafba89-7d34-45a1-8a0b-92829e960b72

--- jc/jc_main.ml
+++ jc/jc_main.ml
@@ -1,6 +1,7 @@
 
 open Jc_env
 open Jc_ast
+open Format
 
 let test1 =
   Jc_interp.statement
@@ -10,8 +11,10 @@ let test1 =
   Output.Def("test1",test1)
   
 let main () =
+  (* banner *)
+  printf "Jessie - Why front-end common to Java and C@.";
   Pp.print_in_file 
-    (fun fmt -> Format.fprintf fmt "%a@." Output.fprintf_why_decl test1)
+    (fun fmt -> fprintf fmt "%a@." Output.fprintf_why_decl test1)
     "/tmp/jessie_test1.why"
 
 
