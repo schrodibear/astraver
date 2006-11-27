786d2012a4c1a4f50286aa37ff473b0cbdad69ad
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2006-11-27 14:16:17 +0000

    un petit invariant
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@1885 ecafba89-7d34-45a1-8a0b-92829e960b72

--- bench/c/good/pi.c
+++ bench/c/good/pi.c
@@ -8,6 +8,7 @@ f[b]=d%--g,d/=g--,--b;d*=b);}
 /*@ axiom simplify_dumb_2 : \forall int c; c*2>0 => c*2>1 */
 /*@ axiom simplify_dumb_3 : \forall int c; c%14==0 => (c-14)%14==0 */
 /*@ axiom simplify_dumb_4 : \forall int c; c%14==0 => c>0 => c>=14 */
+/*@ axiom simplify_dumb_5 : 10000 / 5 == 2000 */
 
 void print4(int);
 
@@ -15,7 +16,7 @@ int a=10000,b,c=2800,d,e,f[2801],g;
 
 /*@ requires b == 0 && c == 2800 && a == 10000 */
 void main(){
-  /*@ invariant 0 <= b <= 2800 
+  /*@ invariant 0 <= b <= 2800 && \forall int i; 0 <= i < b => f[i] == 2000
       variant c-b */
   for(; b-c; b++) 
     f[b] = a/5;
@@ -26,15 +27,15 @@ void main(){
     /*@ invariant 1 <= b <= c && g == b*2
         variant b */
     for(b=c; 1; ) {
-      d+=f[b]*a;
-      f[b]=d%--g;
-      d/=g--;
+      d += f[b]*a;
+      f[b] = d % --g;
+      d /= g--;
       --b;
       if (!b) break; 
-      d*=b;
+      d *= b;
     }
-    c-=14;
+    c -= 14;
     print4(e+d/a);
-    e=d%a;
+    e = d%a;
   }
 }
