4ee036b4412c3537ab9de2e0895e58c35e3f97ac
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2006-12-04 22:04:33 +0000

    count_bits
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@1899 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/c/good/count_bits.c
@@ -0,0 +1,44 @@
+
+// the i-th bit of x as 0 or 2^i
+//@ logic int bit(int x, int i)
+
+// the number of bits 1 in x
+//@ logic int nbits(int x)
+
+//@ axiom nbits_nonneg : \forall int x; nbits(x) >= 0
+
+//@ axiom nbits_zero : nbits(0) == 0
+
+// the lowest bit 1 in x
+//@ logic int lowest_bit(int x)
+
+/*@ axiom lowest_bit_def : 
+  @   \forall int x; x != 0 => 
+  @     \exists int i; (i >= 0 && 
+  @                     lowest_bit(x) == bit(x,i) &&
+  @                     bit(x,i) != 0 &&
+  @                     \forall int j; 0 <= j < i => bit(x,j) == 0)
+  @*/
+
+/*@ axiom lowest_bit_zero :
+  @   \forall int x; lowest_bit(x) == 0 <=> x == 0
+  @*/
+
+/*@ axiom lowest_bit_trick :
+  @   \forall int x; x & -x == lowest_bit(x)
+  @*/
+
+/*@ axiom remove_one_bit :
+  @    \forall int x, int i; 
+  @       bit(x,i) != 0 => nbits(x - bit(x,i)) == nbits(x) - 1
+  @*/
+
+/*@ ensures \result == nbits(x) */
+int count_bits(int x) {
+  int d = 0, c = 0;
+  /*@ invariant c + nbits(x-d) == nbits(\at(x,init))
+    @ variant   nbits(x-d)
+    @*/
+  while (d = (x-=d) & -x) c++;
+  return c;
+}
