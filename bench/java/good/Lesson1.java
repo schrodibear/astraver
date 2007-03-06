62cf16930155fae4b7367d96af4203d3ae9c9c51
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2007-03-06 11:37:34 +0000

    java
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@2102 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/java/good/Lesson1.java
@@ -0,0 +1,49 @@
+
+/*@ axiom distr_right: 
+  @   \forall int x,y,z; x*(y+z) == (x*y)+(x*z)
+  @*/
+
+/*@ axiom distr_left: 
+  @   \forall int x,y,z; (x+y)*z == (x*z)+(y*z)
+  @*/
+
+public class Lesson1 {
+
+    /*@ behavior result_ge_x:
+      @   ensures \result >= x 
+      @ behavior result_ge_y:
+      @   ensures \result >= y 
+      @ behavior result_is_lub:
+      @   ensures \forall int z; z >= x && z >= y => z >= \result
+      @*/
+    public static int max(int x, int y) {
+	if (x>y) return x; else  return y; 
+    }
+    
+
+    /*@ requires x >= 0
+      @ behavior result_is_sqrt: 
+      @   ensures \result >=0 && \result * \result <= x 
+      @      && x < (\result + 1) * (\result + 1)
+      @*/
+    public static int sqrt(int x) {
+	int count = 0, sum = 1;
+	/*@ loop_invariant 
+	  @   count >= 0 && x >= count*count &&
+          @   sum == (count+1)*(count+1);
+	  @ decreases x - sum;
+	  @*/
+	while (sum <= x) { 
+	    count++;
+	    sum = sum + 2*count+1;
+	}
+	return count;
+    }   
+  
+}
+
+/*
+Local Variables: 
+compile-command: "make Lesson1"
+End: 
+*/
