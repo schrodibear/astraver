af493254ddf5ab94eb00a55f8f5173e6004ec907
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2007-08-02 08:49:12 +0000

    *** empty log message ***
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@2424 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/c/good/mean.c
@@ -0,0 +1,28 @@
+
+/* right way to compute the mean of two integers without overflow
+   (extracted from binary_search.c) */
+
+//@ axiom mean_1 : \forall int x; 0 <= x => 0 <= x/2 <= x
+
+//@ requires 0 <= l <= r 
+int mean(int l, int r) {
+  return l + (r - l)/2;
+}
+
+//@ requires l <= r 
+unsigned int umean(unsigned int l, unsigned int r) {
+  //@ assert 0 <= (r-l)/2 <= r-l
+  return l + (r - l)/2;
+}
+
+//@ ensures (\result == x <= y) || (\result == y <= x)
+int min(int x, int y);
+
+//@ ensures (\result == x >= y) || (\result == y >= x)
+int max(int x, int y);
+
+//@ requires 0 <= x && 0 <= y
+int mean2(int x, int y) {
+  int min = min(x,y), max = max(x,y);
+  return min + (max - min)/2;
+}
