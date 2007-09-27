c7a3ca6dfc541fc20e4ffe009a974e559987b79b
filliatr <filliatr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2007-09-27 11:14:39 +0000

    *** empty log message ***
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@2534 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/java/good/BinarySearch.java
@@ -0,0 +1,47 @@
+
+/*@ axiom mean_1 : 
+      \forall int x; \forall int y; x <= y ==> x <= x+(y-x)/2 <= y; */
+
+class BinarySearch {
+
+
+    /* binary_search(t,n,v) search for element v in array t 
+       between index 0 and n-1
+       array t is assumed sorted in increasing order
+       returns an index i between 0 and n-1 where t[i] equals v, 
+       or -1 if no element of t is equal to v  
+    */
+    
+    /*@ requires 
+      @   0 <= n <= t.length &&
+      @   \forall int k1; \forall int k2; 
+      @      0 <= k1 <= k2 <= n-1 ==> t[k1] <= t[k2];
+      @ behavior correctness:
+      @ ensures
+      @   (\result >= 0 && t[\result] == v) ||
+      @   (\result == -1 && \forall int k; 0 <= k < n ==> t[k] != v);
+      @*/
+    int binary_search(int t[], int n, int v) {
+	int l = 0, u = n-1;
+	/*@ loop_invariant 
+	  @   0 <= l && u <= n-1 && 
+	  @   \forall int k; 0 <= k < n ==> t[k] == v ==> l <= k <= u;
+	  @ decreases 
+	  @   u-l ;
+	  @*/
+	while (l <= u ) {
+	    int m = l + (u - l) / 2;
+	    if (t[m] < v) l = m + 1;
+	    else if (t[m] > v) u = m - 1;
+	    else return m; 
+	}
+	return -1;
+    }
+
+}
+    
+/*
+Local Variables: 
+compile-command: "make BinarySearch.io"
+End: 
+*/
