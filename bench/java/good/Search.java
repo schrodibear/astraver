33bae4b56638165c8e48dea27af210d0ee571aca
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2007-10-09 11:36:44 +0000

    add
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@2571 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/java/good/Search.java
@@ -0,0 +1,47 @@
+
+/*@ axiom mean_property : 
+  @   \forall integer x, y; x <= y ==> x <= (x+y)/2 && (x+y)/2 <= y; */
+
+
+/*@ predicate sorted(long t[]) {
+  @   \forall integer i,j; 0 <= i && i <= j && j < t.length ==>
+  @          t[i] <= t[j] } */
+
+public class Search {
+
+    long t[];
+    //@ invariant t_sorted: t != null && sorted(t);
+
+    /*@ requires t != null && t.length < 2147483647 && sorted(t);
+      @ behavior search_success:
+      @   ensures \result >= 0 ==> t[\result] == v;
+      @ behavior search_failure:
+      @   ensures \result < 0 ==> 
+      @     \forall integer k; 0 <= k && k < t.length ==> t[k] != v;
+      @*/
+    int binary_search(long v) {
+	int l = 0, u = t.length-1;
+	/*@ loop_invariant 
+	  @   0 <= l && u <= t.length-1 &&
+	  @     \forall integer i; 
+	  @        0 <= i && i < t.length ==> t[i] == v ==> l <= i && i <= u;
+	  @ decreases u-l ;
+	  @*/
+	while (l <= u ) {
+	    int m = (l + u) / 2;
+	    //@ assert l <= m && m <= u;
+	    if (t[m] < v) l = m + 1;
+	    else if (t[m] > v) u = m - 1;
+	    else return m;
+	}
+	return -1;
+    }
+}
+
+
+/*
+Local Variables: 
+compile-command: "make Search"
+End: 
+*/
+
