415ceacf6f0c7d6c4bf79c7cd503b8a759c124e5
bardou <bardou@ecafba89-7d34-45a1-8a0b-92829e960b72>
2007-12-17 10:30:58 +0000

    Jessica, ml_type.ml: native types finished
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@2782 ecafba89-7d34-45a1-8a0b-92829e960b72

--- ml/ml_type.ml
+++ ml/ml_type.ml
@@ -2,7 +2,9 @@
 
 open Ml_misc
 open Jc_env
+open Jc_output
 open Ml_ocaml.Types
+open Ml_ocaml.Ident
 open Format
 
 let rec print_type fmt t =
@@ -59,29 +61,109 @@ type ml_constructor_info = {
   ml_ci_arguments: Jc_env.field_info list;
 }
 
+module ComparableCamlTypeList = struct
+  type t = type_expr list
+  let compare = Pervasives.compare (* maybe this should be changed *)
+end
+
+module ParamMap = Map.Make(ComparableCamlTypeList)
+
+type ml_jessie_type =
+  | MLTnative of native_type
+  | MLTrecord of Jc_env.struct_info * (string * ml_label_info) list
+  | MLTvariant of Jc_env.struct_info * Jc_env.field_info *
+      (string * ml_constructor_info) list
+
+type ml_caml_type = {
+  ml_ty_name: string;
+  ml_ty_decl: Ml_ocaml.Types.type_declaration;
+  mutable ml_ty_instances: ml_jessie_type ParamMap.t;
+}
+
+let ml_types = Hashtbl.create 11
+
 let declare id td =
-  log "Type declaration parameters:";
-  List.iter (fun t -> print_type t) td.type_params;
-  match td.type_kind with
-    | Type_abstract -> log "Type_abstract"
-    | Type_variant(cl, _) ->
-	List.iter
-	  (fun (n, args) ->
-	     log "Constructor %s:" n;
-	     List.iter print_type args) cl
-    | Type_record(fl, _, _) ->
-	List.iter
-	  (fun (n, _, t) ->
-	     log "Label %s:" n;
-	     print_type t) fl
+  let n = name id in
+  Hashtbl.add ml_types n {
+    ml_ty_name = n;
+    ml_ty_decl = td;
+    ml_ty_instances = ParamMap.empty;
+  }
 
-let make mlt = assert false
+let rec make_type mlt =
+  match mlt.desc with
+    | Tvar -> failwith "ml_type.ml: make_type: Tvar"
+    | Tarrow _ -> failwith "ml_type.ml: make_type: Tarrow"
+    | Ttuple _ -> failwith "ml_type.ml: make_type: Ttuple"
+    | Tconstr(path, args, _) ->
+	begin match Ml_ocaml.Path.name path with
+	  | "unit" -> MLTnative Tunit
+	  | "int" -> MLTnative Tinteger
+	  | "float" -> MLTnative Treal
+	  | "bool" -> MLTnative Tboolean
+	  | name ->
+	      let ty = Hashtbl.find ml_types name in
+	      failwith ("found type "^ty.ml_ty_name)
+	end
+    | Tobject _ -> failwith "ml_type.ml: make_type: Tobject"
+    | Tfield _ -> failwith "ml_type.ml: make_type: Tfield"
+    | Tnil -> failwith "ml_type.ml: make_type: Tnil"
+    | Tlink t -> make_type t
+    | Tsubst _ -> failwith "ml_type.ml: make_type: Tsubst"
+    | Tvariant _ -> failwith "ml_type.ml: make_type: Tvariant"
+    | Tunivar -> failwith "ml_type.ml: make_type: Tunivar"
+    | Tpoly _ -> failwith "ml_type.ml: make_type: Tpoly"
 
-let label ld = assert false
+let make mlt =
+  match make_type mlt with
+    | MLTnative t ->
+	JCTnative t
+    | MLTrecord(si, _)
+    | MLTvariant(si, _, _) ->
+	JCTpointer(si, Some(Num.num_of_int 0), Some(Num.num_of_int 0))
 
-let constructor cd = assert false
+let label ld =
+  match make_type ld.lbl_res with
+    | MLTrecord(_, lbls) -> List.assoc ld.lbl_name lbls
+    | _ -> failwith "ml_type.ml: label: not a record type"
 
-let jc_decls () = assert false
+let constructor cd =
+  match make_type cd.cstr_res with
+    | MLTvariant(_, _, constrs) -> List.assoc cd.cstr_name constrs
+    | _ -> failwith "ml_type.ml: constructor: not a variant type"
+
+let jc_decl name = function
+  | MLTnative t ->
+      JClogic_type_def name
+  | MLTrecord(si, lbls) ->
+      JCstruct_def(
+	si.jc_struct_info_name,
+	(match si.jc_struct_info_parent with
+	   | None -> None
+	   | Some si -> Some si.jc_struct_info_name),
+	List.map (fun (_, l) -> l.ml_li_field) lbls,
+	[] (* TODO: invariants *)
+      )
+  | MLTvariant(si, tagfi, constrs) ->
+      JCstruct_def(
+	si.jc_struct_info_name,
+	(match si.jc_struct_info_parent with
+	   | None -> None
+	   | Some si -> Some si.jc_struct_info_name),
+	tagfi::
+	  List.flatten (List.map (fun (_, l) -> l.ml_ci_arguments) constrs),
+	[] (* TODO: invariants *)
+      )
+
+let jc_decls () =
+  Hashtbl.fold
+    (fun name ty acc ->
+       ParamMap.fold
+	 (fun _ ty acc -> (jc_decl name ty)::acc)
+	 ty.ml_ty_instances
+	 acc)
+    ml_types
+    []
 
 (*
 Local Variables: 
