5309146dde43154a51c92e684ca54d8c733e7746
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2008-03-19 08:49:29 +0000

    ag
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@3057 ecafba89-7d34-45a1-8a0b-92829e960b72

--- bench/java/good/PersistentArray.java
+++ bench/java/good/PersistentArray.java
@@ -1,46 +1,63 @@
 
+//@ type larray
 
-//@ type logic_tab;
-
-/* [create_logic_tab n x] returns an array indexed from 0 to n-1, 
- * with all cells equals to [x] 
+/* [create(x)] returns an array  
+ * where each cell contains [x] 
  **/
-//@ logic logic_tab create_logic_tab(n: integer, x:double);
+//@ logic larray create(double x);
 
-/*
+//@ logic double select(larray t, integer i);
 
-interface PersistentArrayInterface {
+/*@ logic larray 
+  @  store(larray t, integer i, double x);
+  @*/
 
-    //@ model logic_tab model_tab;
+/*@ axiom select_store_eq:
+  @  \forall larray t, integer i, double x;
+  @   select(store(t,i,x),i) == x;
+  @*/
+
+/*@ axiom select_store_neq:
+  @  \forall larray t, integer i, integer j;
+  @   \forall double x;
+  @    i != j ==> 
+  @     select(store(t,i,x),j) == select(x,j);
+  @*/
+
+
+interface PArrayInterface {
+
+    //@ model larray model_array;
+    //@ model integer model_length;
   
     /*@ requires n >= 0;
-      @ ensures \result.model_tab == create_logic(n,0.0);
-      @* /
-    static PersistentArray create(int n);
-
+      @ ensures \result.model_array == create(0.0);
+      @ ensures \result.model_length == n;
+      @*/
+    static PArrayInterface create(int n);
     
-    /*@ requires 0 <= i < length(model_tab);
+    /*@ requires 0 <= i < this.model_length;
       @ assigns \nothing;
-      @ ensures \result == select(this.model_tab,i);
-      @* /
+      @ ensures \result == select(this.model_array,i);
+      @*/
     double get(int i);
 
-    /*@ requires 0 <= i < length(model_tab);
+    /*@ requires 0 <= i < this.model_length;
       @ assigns \nothing;
       @ ensures \fresh(\result); 
-      @ ensures \result.model_tab == store(this.model_tab,i,x);
-      @* /
-    PersistentArray set(int i, double x);
+      @ ensures \result.model_array == store(this.model_array,i,x);
+      @ ensures \result.model_length == this.model_length;
+      @*/
+    PArrayInterface set(int i, double x);
 
 }
 
-*/
 
 abstract class Data {
 
     abstract double get(int i);
     
-    abstract PersistentArray set(int i, double x, PersistentArray parent);
+    abstract PArray set(int i, double x, PArray parent);
 
 }
 
@@ -49,6 +66,10 @@ class Arr extends Data {
     double table[];
 
     //@ invariant table != null;
+    /*@ invariant model_length == table.length &&
+      @   \forall integer i; 
+      @     0 <= i < table.length ==> table[i] == get(model_array,i);
+      @*/
 
     Arr(int n) {
 	table = new double[n];
@@ -58,25 +79,33 @@ class Arr extends Data {
 	return table[i];
     }
 
-    PersistentArray set(int i, double x, PersistentArray parent) {
+    PArray set(int i, double x, PArray parent) {
 	double old = table[i];
 	table[i] = x;
-	PersistentArray tmp1 = new PersistentArray(this);
+	PArray tmp1 = new PArray(this);
 	Data tmp2 = new Diff(i,old,tmp1);
 	parent.contents = tmp2;
 	return tmp1;
+    }
+
+    String toString() {
+	String s = "Arr [";
+	for (int i = 0; i < table.length; i++)
+	    s += i + "; ";
+	return (s + "]");
     }
 }
 
 class Diff extends Data {
 
-    int index;
-    double value;
-    PersistentArray remaining;
+    private int index;
+    private double value;
+    private PArray remaining;
 
     //@ invariant remaining != null;
+    //@ invariant repr(model_length.store(index,value,remaining.model_array),this)
 
-    Diff(int i, double x, PersistentArray rem) {
+    Diff(int i, double x, PArray rem) {
 	index = i;
 	value = x;
 	remaining = rem;
@@ -87,41 +116,52 @@ class Diff extends Data {
 	return remaining.get(i);
     }
 
-    PersistentArray set(int i, double x, PersistentArray t) {
+    PArray set(int i, double x, PArray t) {
 	Data tmp = new Diff(i,x,t);
-	return new PersistentArray(tmp);
+	return new PArray(tmp);
+    }
+
+    String toString() {
+	return "Diff " + index + ", " + value + ", " + remaining;
     }
 }
 
-public class PersistentArray // implements PersistentArrayInterface 
- {
-
-    Data contents;
-
+public class PArray implements PArrayInterface 
+{
+    
+    private Data contents;
+    
     //@ invariant contents != null;
-
-    PersistentArray(Data d) {
+    //@ invariant repr(model_length,model_array,contents);
+    
+    PArray(Data d) {
 	contents = d;
     }
-
-    static PersistentArray create(int n) {
-	return new PersistentArray(new Arr(n));
+    
+    static PArray create(int n) {
+	return new PArray(new Arr(n));
     }
-
+    
     double get(int i) {
 	return contents.get(i);
     }
 
-    PersistentArray set(int i, double x) {
+    PArray set(int i, double x) {
 	return contents.set(i,x,this);
+    }
+
+    String toString() {
+	return ("-> " + contents);
     }
 
     public static void main(String argv[]) {
 
-	PersistentArray t1 = PersistentArray.create(10);
-	PersistentArray t2 = t1.set(5,0.2);
-	System.out.println("t1[5] = " + t1.get(5));
-	System.out.println("t2[5] = " + t2.get(5));
+	PArray t1 = PArray.create(4);
+	PArray t2 = t1.set(0,2.2);
+	PArray t3 = t2.set(1,3.3);
+	System.out.println("t1 = " + t1);
+	System.out.println("t2 = " + t2);
+	System.out.println("t3 = " + t3);
     }
 
 }
