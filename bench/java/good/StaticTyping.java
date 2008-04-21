ce11e0401becdc415b17d03e9ed7baf754f2033a
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2008-04-21 09:19:18 +0000

    m
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@3252 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ bench/java/good/StaticTyping.java
@@ -0,0 +1,27 @@
+
+//@+ NonNullByDefault = all
+
+class C { 
+    public int x;
+}
+
+class StaticTyping {
+
+    C a;
+
+    void m1(C c) {
+	a = null; // bug jessie : OP non generee
+	C b = c; // b pas marque "non null"...
+	b.x = 0; // ..du coup : PO inutile
+    }
+
+
+    void m2(C c) {
+	m1(c);
+    }
+
+}
+    
+
+
+    
