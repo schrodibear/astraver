503baba8977a7cea9428c823961b2873435e1593
signoles <signoles@ecafba89-7d34-45a1-8a0b-92829e960b72>
2008-05-19 14:28:32 +0000

    - inclusion du document d'architecture dans la doc du developpeur - extension du manuel de reference du guide du developpeur
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@3270 ecafba89-7d34-45a1-8a0b-92829e960b72

--- lib/coq-v7/caduceus_why.v
+++ lib/coq-v7/caduceus_why.v
@@ -656,9 +656,12 @@ Implicit Arguments separation1.
   := ((p:(pointer A781))
       ((a:alloc_table)
        ((valid a p) ->
-        ((i:Z)
-         (`0 <= i` /\ `i < size` ->
-          ~((base_addr (acc m1 (shift p i))) = (base_addr (acc m2 p)))))))).
+        ((i1:Z)
+         ((i2:Z)
+          (`0 <= i1` /\ `i1 < size` ->
+           (`0 <= i2` /\ `i2 < size` ->
+            ~((base_addr (acc m1 (shift p i1))) =
+            (base_addr (acc m2 (shift p i2))))))))))).
 Implicit Arguments separation1_range1.
 
 (*Why predicate*) Definition separation1_range [A783:Set] [A782:Set] [m:(memory (pointer A782) A783)] [size:Z]
@@ -666,12 +669,8 @@ Implicit Arguments separation1_range1.
       ((a:alloc_table)
        ((valid a p) ->
         ((i1:Z)
-         ((i2:Z)
-          (`0 <= i1` /\ `i1 < size` ->
-           (`0 <= i2` /\ `i2 < size` ->
-            (`i1 <> i2` ->
-             ~((base_addr (acc m (shift p i1))) =
-             (base_addr (acc m (shift p i2)))))))))))).
+         (`0 <= i1` /\ `i1 < size` ->
+          ~((base_addr (acc m (shift p i1))) = (base_addr (acc m p)))))))).
 Implicit Arguments separation1_range.
 
 (*Why predicate*) Definition separation2 [A785:Set] [A784:Set] [m1:(memory (pointer A784) A785)] [m2:(memory (pointer A784) A785)]
