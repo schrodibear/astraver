6dd4d3408c6d6d7bfd8e0123d6e494a3cb0de24d
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2008-10-15 09:29:27 +0000

    removed inconsistant axiom on modulo
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@3557 ecafba89-7d34-45a1-8a0b-92829e960b72

--- bench/java/good/BinarySearch.java
+++ bench/java/good/BinarySearch.java
@@ -27,7 +27,7 @@
 /*                                                                        */
 /**************************************************************************/
 
-//@+ CheckArithOverflow = no
+//@+ CheckArithOverflow = yes
 
 /*@ lemma mean_property : 
   @   \forall integer x y; x <= y ==> x <= (x+y)/2 <= y ;
