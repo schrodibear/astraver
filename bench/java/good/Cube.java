71e56edf43cc1914089479217de55a06b42808be
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2009-02-05 12:40:25 +0000

    up
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@3671 ecafba89-7d34-45a1-8a0b-92829e960b72

--- bench/java/good/Cube.java
+++ bench/java/good/Cube.java
@@ -1,15 +1,37 @@
+//@+ CheckArithOverflow = no
+
+/*@ lemma distr_right: 
+  @   \forall integer x y z; x*(y+z) == (x*y)+(x*z);
+  @*/
+
+/*@ lemma distr_left: 
+  @   \forall integer x y z; (x+y)*z == (x*z)+(y*z);
+  @*/
+
+/*@ lemma distr_right2: 
+  @   \forall integer x y z; x*(y-z) == (x*y)-(x*z);
+  @*/
+
+/*@ lemma distr_left2: 
+  @   \forall integer x y z; (x-y)*z == (x*z)-(y*z);
+  @*/
+
 public class Cube {
-  //@ requires x > 0;
-  //@ ensures \result == x * x * x;
+    /*@ requires x > 0;
+      @ ensures \result == x * x * x;
+      @*/
   public static int cube(int x) {
     int a = 1;
     int b = 0;
     int c = x;
     int z = 0;
-    //@ maintaining a == 3*(x-c) + 1;
-    //@ maintaining b == 3*(x-c)*(x-c);
-    //@ maintaining z == (x-c)*(x-c)*(x-c);
-    //@ decreasing c;
+    /*@ loop_invariant 
+      @    0 <= c &&
+      @    a == 3*(x-c) + 1 &&
+      @    b == 3*(x-c)*(x-c) &&
+      @    z == (x-c)*(x-c)*(x-c);
+      @ loop_variant c;
+      @*/
     while (c > 0) {
       z += a + b;
       b += 2*a + 1;
