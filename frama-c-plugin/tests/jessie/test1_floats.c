af43c8198e00655f6507d39ab8cd986e7077dbad
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2009-09-15 15:16:24 +0000

    floats
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@3873 ecafba89-7d34-45a1-8a0b-92829e960b72

--- frama-c-plugin/tests/jessie/test1_floats.c
+++ frama-c-plugin/tests/jessie/test1_floats.c
@@ -20,21 +20,24 @@ float cosine2(float x) {
 }
 
 /*@ lemma abs_triangle: \forall real x,y,z;
-  @            \abs(x-y) <= \abs(x-z)+\abs(z-y);
+  @           \abs(x-y) <= \abs(x-z)+\abs(z-y);
+  @ lemma cos_lipschitz: \forall real x,y;
+  @           \abs(\cos(x)-\cos(y)) <= \abs(x-y); 
   @*/
 
 /*@ requires \abs(\exact(x)) <= 0x1p-5 
   @     && \round_error(x) <= 0x1p-20;  
   @ ensures \abs(\exact(\result) - \cos(\exact(x))) <= 0x1p-24
-  @     && \round_error(\result) <= \round_error(x) + 0x0.3p-20;  
+  @     && \round_error(\result) <= \round_error(x) + 0x3p-24;  
   @*/
 float cosine1a(float x) {
-  float r = 1.0f - x * x * 0.5f;
   //@ assert \abs(x) <= 0x1.0002p-5; // by SMT provers and lemma abs_triangle
-  //@ assert \abs(1.0 - \exact(x)*\exact(x)*0.5 - \cos(\exact(x))) <= 0x1p-24;  // by interval
+  float r = 1.0f - x * x * 0.5f;
+  //@ assert \abs(\exact(r) - \cos(\exact(x))) <= 0x1p-24;  // by interval
   //@ assert \abs(1.0 - x*x*0.5 - \cos(x)) <= 0x1p-24;                          // by interval
-  //@ assert \abs(r - \cos(x)) <= 0x1p-23;                          // by gappa
-  /*@ assert \round_error(r) <= \abs(r - \cos(x)) + 
+  //@ assert \abs(r - \cos(x)) <= 0x1p-23;     
+                                // by gappa and previous assertion
+  /* @ assert \round_error(r) <= \abs(r - \cos(x)) + 
     @                           \abs(\cos(x)-\cos(\exact(x))) + 
     @                           \abs(\cos(\exact(x)) - \exact(r));   // by SMT provers and lemmas above
     @*/
