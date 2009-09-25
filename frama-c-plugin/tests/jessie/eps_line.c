40211c38e7de0539c1e69b5d07511108d6c6e0e9
nguyen <nguyen@ecafba89-7d34-45a1-8a0b-92829e960b72>
2009-09-25 15:04:18 +0000

    modify test/jessie/eps_line.c
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@3889 ecafba89-7d34-45a1-8a0b-92829e960b72

--- frama-c-plugin/tests/jessie/eps_line.c
+++ frama-c-plugin/tests/jessie/eps_line.c
@@ -1,33 +1,29 @@
 
-// @ lemma a1: \forall integer x,y; x*y != 0 ==> x!=0 && y!=0;
-
 #pragma JessieFloatModel(multirounding)
-#pragma JessieIntegerModel(math)
-// #pragma JessieFloatModel(strict)
+//#pragma JessieIntegerModel(math)
 
 
-//@ logic integer signe(real x) = (x >= 0.0) ? 1 : -1;
+//@ logic integer l_sign(real x) = (x >= 0.0) ? 1 : -1;
 
 
-
-/*@ requires \abs(x-\exact(x)) <= M;
-  @ ensures (\result != 0 ==> \result == signe(\exact(x))) &&
+/*@ requires e1<= x-\exact(x) <= e2 && -0x1p+1022 <= e1 <= 0 &&
+  @          0<= e2 <=0x1p+1023     && \abs(x)<=0x1p+1023 ;
+  @                                
+  @ ensures (\result != 0 ==> \result == l_sign(\exact(x))) &&
   @          \abs(\result) <= 1 ;
   @*/
-int sign(double x, double M) {
-   double m = -M;
-  //@ assert  m == -M;
-  if (x > M)
+int sign(double x, double e1, double e2) {
+  
+  if (x > e2)
     return 1;
-  if (x < m)
+  if (x < e1)
     return -1;
   return 0;
 }
 
-// 
+ 
 /*@ requires 
   @   sx == \exact(sx) &&
-
   @   sy == \exact(sy) &&
   @   vx == \exact(vx) &&
   @   vy == \exact(vy) &&
@@ -37,22 +33,27 @@ int sign(double x, double M) {
   @   \abs(vy) <= 1.0; 
   @ ensures
   @    \result != 0 
-  @      ==> \result == signe(\exact(sx)*\exact(vx)+\exact(sy)*\exact(vy))
-  @            * signe(\exact(sx)*\exact(vy)-\exact(sy)*\exact(vx));
+  @      ==> \result == l_sign(\exact(sx)*\exact(vx)+\exact(sy)*\exact(vy))
+  @            * l_sign(\exact(sx)*\exact(vy)-\exact(sy)*\exact(vx));
   @*/
 
 int eps_line(double sx, double sy, 
 	     double vx, double vy) {
   int s1,s2;
 
-   s1=sign(sx*vx+sy*vy, 0x1.9000000001ap-45 );
+  s1=sign(sx*vx+sy*vy, -0x1.9000000001ap-45,0x1.9000000001ap-45);
 
   /*@ assert \abs(s1) <= 1 && 
-      (s1 != 0 ==> s1 == signe(\exact(sx)*\exact(vx)+\exact(sy)*\exact(vy))); */   
-    s2=sign(sx*vy-sy*vx, 0x1.9000000001ap-45 );
+    @ (s1 != 0 ==> s1 == l_sign(\exact(sx)*\exact(vx)
+    @                          +\exact(sy)*\exact(vy))); 
+    @*/   
+  s2=sign(sx*vy-sy*vx, -0x1.9000000001ap-45, 0x1.9000000001ap-45);
 
   /*@ assert \abs(s2) <= 1 &&
-      (s2 != 0 ==> s2 == signe(\exact(sx)*\exact(vy)-\exact(sy)*\exact(vx))); */
+    @ (s2 != 0 
+    @   ==> s2 == l_sign(\exact(sx)*\exact(vy)
+    @                          -\exact(sy)*\exact(vx))); 
+    @*/
   
   return s1*s2;
 }
