2ec375b12a1b03a7198199cc14ed7b8b829ba366
nguyen <nguyen@ecafba89-7d34-45a1-8a0b-92829e960b72>
2009-09-28 13:48:20 +0000

    Modify tests/jessie/eps_line.c
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@3892 ecafba89-7d34-45a1-8a0b-92829e960b72

--- frama-c-plugin/tests/jessie/eps_line.c
+++ frama-c-plugin/tests/jessie/eps_line.c
@@ -1,15 +1,10 @@
-
 #pragma JessieFloatModel(multirounding)
-//#pragma JessieIntegerModel(math)
-
+#pragma JessieIntegerModel(math)
 
 //@ logic integer l_sign(real x) = (x >= 0.0) ? 1 : -1;
 
-
-/*@ requires e1<= x-\exact(x) <= e2 && -0x1p+1022 <= e1 <= 0 &&
-  @          0<= e2 <=0x1p+1023     && \abs(x)<=0x1p+1023 ;
-  @                                
-  @ ensures (\result != 0 ==> \result == l_sign(\exact(x))) &&
+/*@ requires e1<= x-\exact(x) <= e2;   
+  @ ensures  (\result != 0 ==> \result == l_sign(\exact(x))) &&
   @          \abs(\result) <= 1 ;
   @*/
 int sign(double x, double e1, double e2) {
@@ -20,41 +15,24 @@ int sign(double x, double e1, double e2) {
     return -1;
   return 0;
 }
-
  
 /*@ requires 
-  @   sx == \exact(sx) &&
-  @   sy == \exact(sy) &&
-  @   vx == \exact(vx) &&
-  @   vy == \exact(vy) &&
-  @   \abs(sx) <= 100.0 && 
-  @   \abs(sy) <= 100.0 && 
-  @   \abs(vx) <= 1.0 && 
-  @   \abs(vy) <= 1.0; 
+  @   sx == \exact(sx)  && sy == \exact(sy) &&
+  @   vx == \exact(vx)  && vy == \exact(vy) &&
+  @   \abs(sx) <= 100.0 && \abs(sy) <= 100.0 && 
+  @   \abs(vx) <= 1.0   && \abs(vy) <= 1.0; 
   @ ensures
   @    \result != 0 
   @      ==> \result == l_sign(\exact(sx)*\exact(vx)+\exact(sy)*\exact(vy))
   @            * l_sign(\exact(sx)*\exact(vy)-\exact(sy)*\exact(vx));
   @*/
 
-int eps_line(double sx, double sy, 
-	     double vx, double vy) {
+int eps_line(double sx, double sy,double vx, double vy){
   int s1,s2;
 
-  s1=sign(sx*vx+sy*vy, -0x1.9000000001ap-45,0x1.9000000001ap-45);
-
-  /*@ assert \abs(s1) <= 1 && 
-    @ (s1 != 0 ==> s1 == l_sign(\exact(sx)*\exact(vx)
-    @                          +\exact(sy)*\exact(vy))); 
-    @*/   
+  s1=sign(sx*vx+sy*vy, -0x1.9000000001ap-45, 0x1.9000000001ap-45);
   s2=sign(sx*vy-sy*vx, -0x1.9000000001ap-45, 0x1.9000000001ap-45);
-
-  /*@ assert \abs(s2) <= 1 &&
-    @ (s2 != 0 
-    @   ==> s2 == l_sign(\exact(sx)*\exact(vy)
-    @                          -\exact(sy)*\exact(vx))); 
-    @*/
-  
+ 
   return s1*s2;
 }
 
