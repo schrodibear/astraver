3ad86bdc73541ac62fa7598b78f5bf495cf13796
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2009-10-15 08:19:51 +0000

    bug fix
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@3916 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ frama-c-plugin/tests/jessie/bts0273.c
@@ -0,0 +1,23 @@
+/* Frama-C BTS 0273 
+
+yields:
+
+Uncaught exception: File "jc/jc_interp.ml", line 1819, characters 1-7: Assertion failed
+
+*/
+
+typedef float real;
+struct T26_27 { real _F0 ; real _F1 ; };
+typedef struct T26_27 _T26;
+struct T28_29 { _T26 _F0 ; _T26 _F1 ; };
+typedef struct T28_29 _T28;
+typedef _T28 RR;
+void f(RR const *tab , real *y )
+{ *y = (float )(double )(*((real *)tab + 1)); }
+
+
+/* 
+Local Variables:
+compile-command: "LC_ALL=C frama-c -jessie bts0273.c"
+End:
+*/
