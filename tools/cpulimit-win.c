e5255d1e39acc411e7b4a185459e0d6b4adcb3be
nrousset <nrousset@ecafba89-7d34-45a1-8a0b-92829e960b72>
2009-12-09 08:28:00 +0000

    *** empty log message ***
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@4057 ecafba89-7d34-45a1-8a0b-92829e960b72

--- tools/cpulimit-win.c
+++ tools/cpulimit-win.c
@@ -14,55 +14,57 @@
 /*                                                                        */
 /**************************************************************************/
 
+/* $Id: cpulimit-win.c,v 1.3 2009-12-09 08:28:00 nrousset Exp $ */
+
 #include <windows.h>
 #include <stdio.h>
 #include <errno.h>
 
-int main( int argc, char *argv[] )
-{
-STARTUPINFO si;
-PROCESS_INFORMATION pi;
-int i;
-unsigned ex;
-unsigned long s=0;	// length of args after concat
-char * p;			// command line parameter
-ZeroMemory( &si, sizeof(si) );
-si.cb = sizeof(si);
-ZeroMemory( &pi, sizeof(pi) );
-
-if( argc < 3 ){
-	printf("Usage: %s <time limit in seconds> <command>\n", argv[0]);
+int main(int argc, char *argv[]) {
+  STARTUPINFO si;
+  PROCESS_INFORMATION pi;
+  int i;
+  unsigned ex;
+  unsigned long s = 0; // length of args after concat
+  char * p; // command line parameter
+  ZeroMemory(&si, sizeof(si));
+  si.cb = sizeof(si);
+  ZeroMemory(&pi, sizeof(pi));
+  if (argc < 3) {
+    printf("Usage: %s <time limit in seconds> <command>\n", argv[0]);
     return -1;
-}
-
-// concats argv[2..] into a single command line parameter
-for(i=2;i<argc;i++) s+=strlen(argv[i])+1;
-if (s>32767) { // CreateProcess does not allow more than 32767 bytes for command line parameter
-	printf("%s: Error: parameter's length exceeds CreateProcess limits\n", argv[0]);
+  }
+  // concats argv[2..] into a single command line parameter
+  for (i = 2; i < argc; i++)
+    s += strlen(argv[i]) + 1;
+  // CreateProcess does not allow more than 32767 bytes for command line parameter
+  if (s > 32767) { 
+    printf("%s: Error: parameter's length exceeds CreateProcess limits\n", argv[0]);
     return -1;
-}
-p=(char*)malloc(s);
-if (p==NULL) {
-	printf("%s: Error: when allocating %d bytes in memory\n", argv[0], (int)s);
+  }
+  p = (char*) malloc(s);
+  if (p == NULL) {
+    printf("%s: Error: when allocating %d bytes in memory\n", argv[0], (int) s);
     return -1;
-}
-for(i=2;i<argc;i++) {strncat(p,argv[i],strlen(argv[i])); if(i<argc-1)strcat(p," ");}
-
-// launches "child" process with command line parameter
-if( !CreateProcess(NULL, p, NULL, NULL, FALSE,0, NULL, NULL, &si, &pi) ){
+  }
+  for (i = 2; i < argc; i++) {
+    strncat(p, argv[i], strlen(argv[i])); 
+    if (i < argc - 1) strcat(p, " ");
+  }
+  // launches "child" process with command line parameter
+  if (!CreateProcess(NULL, p, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
     printf( "%s: Error: failed when launching <%s>\n", argv[0], p);
     return -1;
-}
-
-// waits, terminates and frees handles and malloc
-WaitForSingleObject( pi.hProcess, 1000*atoi(argv[1]) );
-TerminateProcess(pi.hProcess,10);
-GetExitCodeProcess(pi.hProcess,(LPDWORD)&ex);
-CloseHandle( pi.hProcess );
-CloseHandle( pi.hThread );
-free(p);
-
-return ex;
+  }
+  // waits, terminates and frees handles and malloc
+  int w = WaitForSingleObject(pi.hProcess, 1000 * atoi(argv[1]));
+  TerminateProcess(pi.hProcess, 10);
+  GetExitCodeProcess(pi.hProcess, (LPDWORD) &ex);
+  CloseHandle(pi.hProcess);
+  CloseHandle(pi.hThread);
+  free(p);
+  if (w == WAIT_TIMEOUT) return 152;
+  return ex;
 }
 
 // How to compile under Cygwin (needs make, gcc and win32api):
