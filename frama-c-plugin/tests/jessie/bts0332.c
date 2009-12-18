38241208d6a59d9c8d621590c214b384379b153c
marche <marche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2009-12-18 16:52:25 +0000

    add
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@4073 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ frama-c-plugin/tests/jessie/bts0332.c
@@ -0,0 +1,24 @@
+/* Frama-C BTS 0329
+
+Status: open
+
+*/
+
+
+
+/*@ requires \valid(p);
+  @ assigns *p;
+  @ ensures *p ==  ((e || (t && f)) && (t || f)) ;
+  @*/
+void f(int*p,int e,int t,int f,int r)
+{
+  *p = ( (e || (t && f)) && (t || f) );
+}
+
+
+
+/*
+Local Variables:
+compile-command: "LC_ALL=C frama-c -jessie bts0329.c"
+End:
+*/
