2622c35c6997fefe2d83c1918c6171857be8825b
filliatr@lri.fr <filliatr@lri.fr@ecafba89-7d34-45a1-8a0b-92829e960b72>
2010-05-10 12:51:18 +0000

    headers de moins de 80 colonnes
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@4249 ecafba89-7d34-45a1-8a0b-92829e960b72

--- bench/c/good/all_zeros.c
+++ bench/c/good/all_zeros.c
@@ -1,33 +1,33 @@
-/********************************************************************************/
-/*                                                                              */
-/*  The Why platform for program certification                                  */
-/*                                                                              */
-/*  Copyright (C) 2002-2010                                                     */
-/*                                                                              */
-/*    Yannick MOY, Univ. Paris-sud 11                                           */
-/*    Jean-Christophe FILLIATRE, CNRS                                           */
-/*    Claude MARCHE, INRIA & Univ. Paris-sud 11                                 */
-/*    Romain BARDOU, Univ. Paris-sud 11                                         */
-/*    Thierry HUBERT, Univ. Paris-sud 11                                        */
-/*                                                                              */
-/*  Secondary contributors:                                                     */
-/*                                                                              */
-/*    Nicolas ROUSSET, Univ. Paris-sud 11 (on Jessie & Krakatoa)                */
-/*    Ali AYAD, CNRS & CEA Saclay         (floating-point support)              */
-/*    Sylvie BOLDO, INRIA                 (floating-point support)              */
-/*    Jean-Francois COUCHOT, INRIA        (sort encodings, hypothesis pruning)  */
-/*    Mehdi DOGGUY, Univ. Paris-sud 11    (Why GUI)                             */
-/*                                                                              */
-/*  This software is free software; you can redistribute it and/or              */
-/*  modify it under the terms of the GNU Lesser General Public                  */
-/*  License version 2.1, with the special exception on linking                  */
-/*  described in file LICENSE.                                                  */
-/*                                                                              */
-/*  This software is distributed in the hope that it will be useful,            */
-/*  but WITHOUT ANY WARRANTY; without even the implied warranty of              */
-/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                        */
-/*                                                                              */
-/********************************************************************************/
+/**************************************************************************/
+/*                                                                        */
+/*  The Why platform for program certification                            */
+/*                                                                        */
+/*  Copyright (C) 2002-2010                                               */
+/*                                                                        */
+/*    Yannick MOY, Univ. Paris-sud 11                                     */
+/*    Jean-Christophe FILLIATRE, CNRS                                     */
+/*    Claude MARCHE, INRIA & Univ. Paris-sud 11                           */
+/*    Romain BARDOU, Univ. Paris-sud 11                                   */
+/*    Thierry HUBERT, Univ. Paris-sud 11                                  */
+/*                                                                        */
+/*  Secondary contributors:                                               */
+/*                                                                        */
+/*    Nicolas ROUSSET, Univ. Paris-sud 11 (on Jessie & Krakatoa)          */
+/*    Ali AYAD, CNRS & CEA Saclay         (floating-point support)        */
+/*    Sylvie BOLDO, INRIA                 (floating-point support)        */
+/*    Jean-Francois COUCHOT, INRIA        (sort encodings, hyps pruning)  */
+/*    Mehdi DOGGUY, Univ. Paris-sud 11    (Why GUI)                       */
+/*                                                                        */
+/*  This software is free software; you can redistribute it and/or        */
+/*  modify it under the terms of the GNU Lesser General Public            */
+/*  License version 2.1, with the special exception on linking            */
+/*  described in file LICENSE.                                            */
+/*                                                                        */
+/*  This software is distributed in the hope that it will be useful,      */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of        */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  */
+/*                                                                        */
+/**************************************************************************/
 
 /* check that t[0..n-1] only contains 0 */
 
