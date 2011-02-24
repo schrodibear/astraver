39f5c7bef8d16e815a9d88c0a14047b134533128
virgile <virgile@ecafba89-7d34-45a1-8a0b-92829e960b72>
2011-02-24 10:28:21 +0000

    update test script wrt changes to ptests
    git-svn-id: https://www.lri.fr/svn/demons/why2/trunk@4535 ecafba89-7d34-45a1-8a0b-92829e960b72

--- frama-c-plugin/tests/jessie/run_test.sh
+++ frama-c-plugin/tests/jessie/run_test.sh
@@ -1,9 +1,9 @@
-suffix=$1
+suffix=$2
 dir=`dirname $0`
-name=`echo $2 | cut -d"." -f1`
-if [ "$name.c" != "$2" ]
+name=`echo $1 | cut -d"." -f1`
+if [ "$name.c" != "$1" ]
 then
-	echo "run_test: name of test \"$2\" should match \"$name.c\""
+	echo "run_test: name of test \"$1\" should match \"$name.c\""
 	exit 1
 fi
 cd "$dir"
