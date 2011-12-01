e360509b2fc4ad4cea847a88e3052156d538e45a
cmarche <cmarche@ecafba89-7d34-45a1-8a0b-92829e960b72>
2011-12-01 10:23:56 +0000

    float arrays
    git-svn-id: https://www.lri.fr/svn/demons/why2/branches/Nitrogen@4797 ecafba89-7d34-45a1-8a0b-92829e960b72

---
+++ tests/c/float_array.c
@@ -0,0 +1,33 @@
+#define EPS1 0x1p-53
+#define UPPER 0x1p0
+#define n 10
+
+//@ predicate bounded(real z, real bound) = \abs(z) <= bound;
+
+double x;
+
+/*@
+  @ requires bounded(d, UPPER);
+  @ requires \round_error(d) == 0;
+  @
+  @ ensures \abs(x - (d + 1.0)) <= EPS1;
+  @ ensures \round_error(x) <= EPS1;
+*/
+void AffectDoubleDansX(double d) {
+        x=d+1.0;
+}
+
+/*@
+  @ requires \valid_range(X,0,n);
+  @ requires bounded(d, UPPER);
+  @ requires \round_error(d) == 0;
+  @
+  @ ensures \abs(X[1] - (d + 1.0)) <= EPS1;
+  @ ensures \round_error(X[1]) <= EPS1;
+  @ ensures \abs(X[2] - (d + 1.0)) <= EPS1;
+  @ ensures \round_error(X[2]) <= EPS1;
+*/
+void AffectDoubleDansTab(double d, double X[]) {
+        X[1]=d+1.0;
+        X[2]=d+1.0;
+}
