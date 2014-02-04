b199f07f2a094088997218089a62883bf42fc43f
Mikhail Mandrykin <mandrykin@ispras.ru>
2014-02-04 19:53:18 +0400

    Improved debug script

--- jc/debug.sh
+++ jc/debug.sh
@@ -4,27 +4,25 @@ DIR=$(dirname $(readlink -f $0))
 
 ocamlc -c -I "$DIR/../src/" -o "$DIR/jc_debug_output.cmo" "$DIR/jc_debug_output.ml"
 
-{
-  echo "load_printer nums.cma"
-  echo "load_printer pp.cmo"
-  echo "load_printer jc_stdlib.cmo"
-  echo "load_printer jc_envset.cmo"
-  echo "load_printer option_misc.cmo"
-  echo "load_printer jc_common_options.cmo"
-  echo "load_printer jc_region.cmo"
-  echo "load_printer jc_fenv.cmo"
-  echo "load_printer loc.cmo"
-  echo "load_printer jc_constructors.cmo"
-  echo "load_printer jc_pervasives.cmo"
-  echo "load_printer jc_output_misc.cmo"
-  echo "load_printer jc_iterators.cmo"
-  echo "load_printer jc_type_var.cmo"
-  echo "load_printer jc_poutput.cmo"
-  echo "load_printer jc_output.cmo"
-  echo "load_printer jc_debug_output.cmo"
-  echo "install_printer Jc_debug_output.expr"
-  echo "install_printer Jc_debug_output.assertion"
-  echo "install_printer Jc_debug_output.term"
-  cat
-} |
-rlwrap ocamldebug -I "$DIR/../src/" -I "$DIR" $@
+SCRIPT='load_printer nums.cma
+load_printer pp.cmo
+load_printer jc_stdlib.cmo
+load_printer jc_envset.cmo
+load_printer option_misc.cmo
+load_printer jc_common_options.cmo
+load_printer jc_region.cmo
+load_printer jc_fenv.cmo
+load_printer loc.cmo
+load_printer jc_constructors.cmo
+load_printer jc_pervasives.cmo
+load_printer jc_output_misc.cmo
+load_printer jc_iterators.cmo
+load_printer jc_type_var.cmo
+load_printer jc_poutput.cmo
+load_printer jc_output.cmo
+load_printer jc_debug_output.cmo
+install_printer Jc_debug_output.expr
+install_printer Jc_debug_output.assertion
+install_printer Jc_debug_output.term
+'
+rlwrap -P "$SCRIPT" ocamldebug -I "$DIR/../src/" -I "$DIR" $@
