93b1cc2f7124ae6b11329b22696e76e1146344b5
Mikhail Mandrykin <mandrykin@ispras.ru>
2019-03-28 22:55:15 +0300

    Updated debug.sh after switch to Dune

--- debug.sh
+++ debug.sh
@@ -1,51 +1,48 @@
 #/bin/bash
 
-BUILDSRC=$(dirname $(readlink -f $0))/_build/src
+DIR=$(dirname $(readlink -f $0))
+BUILDSRC=$DIR/_build/default/src/av/.astraver.objs/byte/
 
-make -C $BUILDSRC/../.. -j byte
-
-ocamlc -c -g -no-alias-deps -open Aliases -I "$BUILDSRC/why" -I "$BUILDSRC/av" -o "$BUILDSRC/av/av_debug_print.cmo" "$BUILDSRC/../../src/av/av_debug_print.ml"
+make -C $DIR -j byte
 
 SCRIPT='load_printer str.cma
 load_printer nums.cma
-load_printer why_pp.cmo
-load_printer av_stdlib.cmo
-load_printer av_envset.cmo
-load_printer av_common_options.cmo
-load_printer av_region.cmo
-load_printer av_fenv.cmo
-load_printer why_loc.cmo
-load_printer av_constructors.cmo
-load_printer av_common.cmo
-load_printer av_print_misc.cmo
-load_printer av_iterators.cmo
-load_printer av_type_var.cmo
-load_printer av_print_p.cmo
-load_printer av_print.cmo
-load_printer av_debug_print.cmo
+load_printer astraver__Pp.cmo
+load_printer astraver__Stdlib.cmo
+load_printer astraver__Envset.cmo
+load_printer astraver__Common_options.cmo
+load_printer astraver__Region.cmo
+load_printer astraver__Fenv.cmo
+load_printer astraver__Loc.cmo
+load_printer astraver__Constructors.cmo
+load_printer astraver__Common.cmo
+load_printer astraver__Print_misc.cmo
+load_printer astraver__Iterators.cmo
+load_printer astraver__Type_var.cmo
+load_printer astraver__Print_p.cmo
+load_printer astraver__Print.cmo
+load_printer astraver__Debug_print.cmo
 
-load_printer av_print_n.d.cmo
-load_printer why_rc.d.cmo
-load_printer why_version.d.cmo
-load_printer av_version.d.cmo
-load_printer av_position.d.cmo
-load_printer av_name.d.cmo
-load_printer av_output.d.cmo
-load_printer av_common_options.d.cmo
-load_printer av_options.d.cmo
-load_printer av_norm.d.cmo
-load_printer av_struct_tools.d.cmo
-load_printer av_typing.d.cmo
-load_printer av_effect.d.cmo
+load_printer astraver__Print_n.cmo
+load_printer astraver__Rc.cmo
+load_printer astraver__Version.cmo
+load_printer astraver__Position.cmo
+load_printer astraver__Name.cmo
+load_printer astraver__Output.cmo
+load_printer astraver__Common_options.cmo
+load_printer astraver__Options.cmo
+load_printer astraver__Norm.cmo
+load_printer astraver__Struct_tools.cmo
+load_printer astraver__Typing.cmo
+load_printer astraver__Effect.cmo
 
-install_printer Av_effect.print_effect
-install_printer Av_debug_print.expr
-install_printer Av_debug_print.assertion
-install_printer Av_debug_print.term
-install_printer Av_debug_print.string_set
-install_printer Av_debug_print.location
-install_printer Av_debug_print.location_set
+install_printer Astraver__Effect.print_effect
+install_printer Astraver__Debug_print.expr
+install_printer Astraver__Debug_print.assertion
+install_printer Astraver__Debug_print.term
+install_printer Astraver__Debug_print.string_set
+install_printer Astraver__Debug_print.location
+install_printer Astraver__Debug_print.location_set
 set print_depth 3
 '
-rlwrap -P "$SCRIPT" ocamldebug -I "$BUILDSRC/why" -I "$BUILDSRC/av" -I "$BUILDSRC/../../src/why" -I "$BUILDSSRC/../../src/av"\
-       $BUILDSRC/../../bin/astraver.byte $@
+rlwrap -P "$SCRIPT" ocamldebug -I "$BUILDSRC" -I "$DIR/src/av" $DIR/bin/astraver.byte $@
